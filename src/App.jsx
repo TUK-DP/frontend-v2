7b3d2d7a5bf90018210a7fd258ca68ca09bc1b44
heedong12 <dlgmldus3289@naver.com>
2024-06-26 17:35:17 +0900

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import "./index.css";
+import "./App.css";
+
+function App() {
+  return <div className="App">hi</div>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          600: "#6100C1",
+        },
+        sliderBgGame: "#FFE68F",
+        sliderBgResult: "#FF9B52",
+        sliderBgExercise: "#FF7D7D",
+        sliderBgCenter: "#356DD9",
+      },
+    },
+  },
+  plugins: [],
+};
