26cf4186ad230dc1aa3e2c0ad43ab42001152af5
heedong12 <dlgmldus3289@naver.com>
2024-06-27 20:21:32 +0900

    [publishing]HeaderBar 디자인

---
+++ src/components/HeaderBar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { IoIosArrowBack } from "react-icons/io";
+import { useNavigate } from "react-router-dom";
+
+const HeaderBar = ({ content }) => {
+  const navigate = useNavigate();
+  const handleClickBackButton = () => {
+    navigate(-1);
+  };
+  return (
+    <div className="w-full h-20 px-3 py-4 flex flex-row items-center">
+      <IoIosArrowBack
+        size={40}
+        color="#B2B2B2"
+        onClick={() => handleClickBackButton()}
+      />
+      {/* 나중에 페이지 이름 넣을 곳 */}
+      {/* <div className={"flex-1 text-center"}>{content}</div> */}
+      <div className={"w-10"}></div>
+    </div>
+  );
+};
+
+export default HeaderBar;
