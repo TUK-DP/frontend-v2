5ba923cd444a4db512019def6d3781287b3c179f
heedong12 <dlgmldus3289@naver.com>
2024-06-27 23:12:34 +0900

    [publishing]중복 변수 선언 제거

--- src/components/signup/SignupStep.jsx
+++ src/components/signup/SignupStep.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { TbCircleNumber1 } from "react-icons/tb";
 import { TbCircleNumber1Filled } from "react-icons/tb";
 import { TbCircleNumber2Filled } from "react-icons/tb";
 import { TbCircleNumber2 } from "react-icons/tb";
 import { TbCircleCheckFilled } from "react-icons/tb";
 
+const INPUT_EMAIL = "이메일 입력";
+const INPUT_INFO = "정보 입력";
 const SigupStep = ({ step }) => {
   return (
     <div className={"w-full flex flex-row justify-center items-center py-5"}>
@@ -17,8 +18,6 @@ const SigupStep = ({ step }) => {
 export default SigupStep;
 
 const Step1ProgressBar = () => {
-  const INPUT_EMAIL = "이메일 입력";
-  const INPUT_INFO = "정보 입력";
   return (
     <>
       <TbCircleNumber1Filled size={30} color="#1777FF" />
@@ -31,8 +30,6 @@ const Step1ProgressBar = () => {
 };
 
 const Step2ProgressBar = () => {
-  const INPUT_EMAIL = "이메일 입력";
-  const INPUT_INFO = "정보 입력";
   return (
     <>
       <TbCircleCheckFilled size={30} color="#1777FF" />
