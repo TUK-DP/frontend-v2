1b801bd4eecbf3f0222ffc745e53174a0ad5f107
Hj1218 <wjdguswl2341@naver.com>
2024-06-27 17:03:17 +0900

    [publishing] 디렉토리 구조 변경

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 import Slider from "react-slick";
-import diaryImg from "../assets/home/diary.png";
-import gameImg from "../assets/home/game.png";
-import gymnasticsImg from "../assets/home/gymnastics.png";
-import centerImg from "../assets/home/center.png";
-import surveyImg from "../assets/home/survey.png";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../styles/sliderStyles.css";
+import diaryImg from "../assets/home/slider/diary.png";
+import gameImg from "../assets/home/slider/game.png";
+import gymnasticsImg from "../assets/home/slider/gymnastics.png";
+import centerImg from "../assets/home/slider/center.png";
+import surveyImg from "../assets/home/slider/survey.png";
 
 export const HOME_PAGE_PATH = "/home";
 
@@ -21,9 +21,9 @@ const Home = () => {
 
 export default Home;
 
+//슬라이더 컴포넌트
 const SimpleSlider = () => {
-  const images = [diaryImg, gameImg, gymnasticsImg, centerImg, surveyImg];
-
+  //슬라이더 설정
   const settings = {
     dots: true,
     infinite: true,
@@ -33,6 +33,14 @@ const SimpleSlider = () => {
     slidesToScroll: 1,
   };
 
+  //이미지 데이터
+  const images = [
+    diaryImg, 
+    gameImg, 
+    gymnasticsImg, 
+    centerImg, 
+    surveyImg
+  ];
   return (
     <div>
       <Slider {...settings}>
