886a56a8bfc875cc0bee63d2bf5e8c1170ecc37d
Hj1218 <wjdguswl2341@naver.com>
2024-06-27 22:36:15 +0900

    [publishing] 슬라이더 이미지 구현

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,11 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "../styles/sliderStyles.css";
-import diaryImg from "../assets/home/slider/diary.png";
-import gameImg from "../assets/home/slider/game.png";
-import gymnasticsImg from "../assets/home/slider/gymnastics.png";
-import centerImg from "../assets/home/slider/center.png";
-import surveyImg from "../assets/home/slider/survey.png";
+import slider_diary from "../assets/home/slider/slider_diary.png";
+import slider_game from "../assets/home/slider/slider_game.png";
+import slider_gymnastics from "../assets/home/slider/slider_gymnastics.png";
+import slider_dementiacenter from "../assets/home/slider/slider_dementiacenter.png";
+import slider_survey from "../assets/home/slider/slider_survey.png";
 import btn_centerMap from "../assets/home/button/btn_centerMap.png";
 import btn_survey from "../assets/home/button/btn_survey.png";
 import btn_diary from "../assets/home/button/btn_diary.png";
@@ -55,19 +55,24 @@ const SimpleSlider = () => {
 
   //이미지 데이터
   const images = [
-    diaryImg, 
-    gameImg, 
-    gymnasticsImg, 
-    centerImg, 
-    surveyImg
+    { imgsrc: slider_diary, sentence1: "일기를 회상하고", sentence2: "나만의 그림을 그려보세요!", color:"bg-[#6100C1]" },
+    { imgsrc: slider_dementiacenter, sentence1: "주변 치매센터가 어디있나요?", sentence2: "가까운 치매센터 찾기", color:"bg-[#356DD9]" },
+    { imgsrc: slider_gymnastics, sentence1: "간단하게 따라할 수 있는", sentence2: "체조들을 만나보세요!", color:"bg-[#FF7D7D]" },
+    { imgsrc: slider_survey, sentence1: "간단한 치매 진단과", sentence2: "이전 결과들을 확인하세요!", color:"bg-[#FF9B52]" },
+    { imgsrc: slider_game, sentence1: "간단한 게임들을 통해", sentence2: "인지력을 강화시켜보세요!", color:"bg-[#FFE68F]" },
   ];
 
   return (
     <div>
       <Slider {...settings}>
         {images.map((image, index) => (
-          <div key={index}>
-            <img src={image} />
+          <div key={index} className={`relative h-64 ${image.color}`}>
+            <div className={`absolute top-12 left-12 text-3xl font-bold ${index === 4 ? 'text-black' : 'text-white'}`} >
+              {image.sentence1}
+              <br/>
+              {image.sentence2}
+            </div>
+            <img src={image.imgsrc} className="absolute right-8 bottom-8"/>
           </div>
         ))}
       </Slider>
