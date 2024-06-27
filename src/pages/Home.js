1eb37d862e5a0dd505718b86c91f57421417e400
Hj1218 <wjdguswl2341@naver.com>
2024-06-27 18:04:37 +0900

    [publishing] 치매진단, 일기장 버튼 추가

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,6 +9,8 @@ import gymnasticsImg from "../assets/home/slider/gymnastics.png";
 import centerImg from "../assets/home/slider/center.png";
 import surveyImg from "../assets/home/slider/survey.png";
 import centerMap from "../assets/home/button/centerMap.png";
+import btn_survey from "../assets/home/button/btn_survey.png";
+import btn_diary from "../assets/home/button/btn_diary.png";
 
 export const HOME_PAGE_PATH = "/home";
 
@@ -16,6 +18,7 @@ const Home = () => {
   return (
     <div>
       <SimpleSlider />
+      <Btn_survey_diary />
       <Btn_center />
     </div>
   );
@@ -56,9 +59,34 @@ const SimpleSlider = () => {
   );
 };
 
+//치매진단, 일기장 버튼 컴포넌트 
+const Btn_survey_diary = () => {
+  return(
+    <div className="flex w-[90%] mx-auto justify-between my-8">
+      <Btn str1={"간단한"} str2={"치매진단"} imgsrc={btn_survey} />
+      <Btn str1={"하루기록"} str2={"일기장"} imgsrc={btn_diary} />
+    </div>
+  );
+};
+
+//버튼 컴포넌트
+const Btn = ({str1, str2, imgsrc}) => {
+  return (
+    <div className="w-[48%] h-[10rem] border-2 rounded-xl bg-[#f4f4f4] relative">
+      <div className="absolute left-4 top-4">
+        <span className="text-[#5B5B5B] text-xl font-bold">{str1}</span>
+        <br/>
+        <span className="text-xl font-bold">{str2}</span>
+      </div>
+      <img src={imgsrc} className="absolute bottom-2 right-2"/>
+    </div>
+  )
+}
+
+//센터 버튼 컴포넌트
 const Btn_center = () => {
   return(
-    <div className="w-[90%] mx-auto my-7 flex justify-between items-center border-2 p-4 rounded-xl">
+    <div className="w-[90%] mx-auto my-7 flex justify-between items-center border-2 p-5 rounded-xl">
       <div>
         <span className="text-xl font-bold">가까운 치매센터 찾기 {'>'}</span>
         <br/>
