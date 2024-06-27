d5a7cfc35cba80fd962758723ecf20d9b37d9f17
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-06-27 17:30:17 +0900

    [feat] useNavBar 제거후 navBar state 전역 상태로 관리

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,32 +1,8 @@
-import { useNavBar } from "../../hooks/useNavBar";
-import { ReactComponent as DiaryIcon } from "../../assets/navbar/diary_icon.svg";
-import { ReactComponent as HomeIcon } from "../../assets/navbar/home_icon.svg";
-import { ReactComponent as MyPageIcon } from "../../assets/navbar/mypage_icon.svg";
 import { NavBarItem } from "./NavBarItem";
-
-export const NAV_BAR_ITEM_LIST = [
-  {
-    value: "diary",
-    name: "일기",
-    icon: DiaryIcon,
-    isSelected: false,
-  },
-  {
-    value: "home",
-    name: "홈",
-    icon: HomeIcon,
-    isSelected: true,
-  },
-  {
-    value: "mypage",
-    name: "마이페이지",
-    icon: MyPageIcon,
-    isSelected: false,
-  },
-];
+import { useNavBarStore } from "../../stores/NavBarStore";
 
 export const NavBar = () => {
-  const { menuList, onClick } = useNavBar({ initState: NAV_BAR_ITEM_LIST });
+  const { menuList, onClick } = useNavBarStore((state) => state);
 
   return (
     <nav
