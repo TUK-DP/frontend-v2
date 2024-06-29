b76ee159e288d356e2572885bff6e60470bd4d4b
heedong12 <dlgmldus3289@naver.com>
2024-06-29 18:26:48 +0900

    [publishing] 버튼 클릭 시, 메소드 실행

--- src/components/signup/PurpleButton.jsx
+++ src/components/signup/PurpleButton.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 
-const PurpleButton = ({ text }) => {
+const PurpleButton = ({ text, handleClickButton }) => {
   return (
     <button
       className={
         "bg-primary-600 w-full h-[46px] rounded-lg-xl text-white text-2xl font-bold"
       }
+      onClick={handleClickButton}
     >
       {text}
     </button>
