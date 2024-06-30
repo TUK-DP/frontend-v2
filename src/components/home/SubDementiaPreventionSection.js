571fffe9bd79be3cdc80d769ac03faaa359b88c6
Hj1218 <wjdguswl2341@naver.com>
2024-06-30 20:55:14 +0900

    [publishing] prettier 적용

--- src/components/home/SubDementiaPreventionSection.js
+++ src/components/home/SubDementiaPreventionSection.js
@@ -9,22 +9,23 @@ const SubDementiaPreventionSection = () => {
       <div className="text-2xl font-bold mt-14 mb-5">치매를 예방해볼까요?</div>
       <GymnasticsButton />
       <div className=" text-md md:text-xl my-5">
-      <span className="text-orange-500">미니게임</span>
-      <span>을 통해 두뇌를 활성화 시키세요!</span>
+        <span className="text-orange-500">미니게임</span>
+        <span>을 통해 두뇌를 활성화 시키세요!</span>
       </div>
       <GameButtonList />
     </>
-  )
-}
-//체조 버튼 컴포넌트 
+  );
+};
+//체조 버튼 컴포넌트
 const GymnasticsButton = () => {
   return (
     <div className="mb-6 flex justify-between items-center border-2 rounded-xl p-5 md:px-12 bg-[#f4f4f4] text-md sm:text-lg md:text-xl cursor-pointer">
       <img src={btn_gymnastics} />
       <div className="mr-8 md:mr-16">
-        <span className="text-orange-500">간단한 체조</span><span>를 통해</span>
-        <br/>
-        <span>치매를 예방하세요 {'>'}</span>
+        <span className="text-orange-500">간단한 체조</span>
+        <span>를 통해</span>
+        <br />
+        <span>치매를 예방하세요 {">"}</span>
       </div>
     </div>
   );
@@ -35,16 +36,19 @@ const GameButtonList = () => {
   //게임 데이터
   const games = [
     { imgsrc: btn_rockScissorsPaper, name: "지는 가위바위보" },
-    { imgsrc: btn_colorMatch, name: "컬러매치" }
+    { imgsrc: btn_colorMatch, name: "컬러매치" },
   ];
 
   return (
     <>
       {games.map((game, index) => (
-        <div key={index} className="h-[4rem] flex justify-between items-center border-2 rounded-xl px-4 mb-5 text-lg md:text-2xl cursor-pointer">
+        <div
+          key={index}
+          className="h-[4rem] flex justify-between items-center border-2 rounded-xl px-4 mb-5 text-lg md:text-2xl cursor-pointer"
+        >
           <img src={game.imgsrc} alt={game.name} width={"50px"} />
           {game.name}
-          <span className="text-blue-600 font-bold">도전 {'>'}</span>
+          <span className="text-blue-600 font-bold">도전 {">"}</span>
         </div>
       ))}
     </>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,21 +17,24 @@ const Home = () => {
       </div>
     </>
   );
-}
+};
 
 export default Home;
 
-
 //치매센터 버튼 컴포넌트
 const DementiaCenterButton = () => {
-  return(
+  return (
     <div className="my-7 flex justify-between items-center border-2 py-5 px-3 md:px-8 rounded-xl cursor-pointer">
       <div>
-        <span className="text-md sm:text-lg md:text-xl font-bold">가까운 치매센터 찾기 {'>'}</span>
-        <br/>
-        <span className="text-xs sm:text-sm md:text-base">주변 치매센터를 확인해보세요</span>
+        <span className="text-md sm:text-lg md:text-xl font-bold">
+          가까운 치매센터 찾기 {">"}
+        </span>
+        <br />
+        <span className="text-xs sm:text-sm md:text-base">
+          주변 치매센터를 확인해보세요
+        </span>
       </div>
-      <img src={btn_centerMap}/>
+      <img src={btn_centerMap} />
     </div>
   );
 };
