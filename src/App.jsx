5f2d23091d91ea3c8bb18dfb2bd3f8d012ab2b13
heedong12 <dlgmldus3289@naver.com>
2024-06-30 13:52:42 +0900

    [publishing] import page path

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import NavigationBarLayout from "./layouts/NavigationBarLayout";
 import { TestApp } from "./pages/TestApp";
 import { MobileResponsiveLayout } from "./layouts/MobileResponsiveLayout";
-import Signup from "./pages/Signup";
-import Signin from "./pages/Signin";
+import Signup, { SIGNUP_PAGE_PATH } from "./pages/Signup";
+import Signin, { SIGNIN_PAGE_PATH } from "./pages/Signin";
 
 function App() {
   return (
@@ -15,8 +15,8 @@ function App() {
           <Route exact element={<NavigationBarLayout />}>
             <Route exact path={"/"} element={<TestApp />} />
           </Route>
-          <Route exact path={"/signup"} element={<Signup />} />
-          <Route exact path={"/signin"} element={<Signin />} />
+          <Route exact path={SIGNUP_PAGE_PATH} element={<Signup />} />
+          <Route exact path={SIGNIN_PAGE_PATH} element={<Signin />} />
         </Route>
       </Routes>
     </BrowserRouter>
