9c041068be2b9ceda2d3ebae671cba82a12e2cd2
Hj1218 <wjdguswl2341@naver.com>
2024-07-04 23:53:20 +0900

    [publishing] DrawingTools컴포넌트 생성

---
+++ src/components/draw/DrawingTools.js
@@ -0,0 +1,43 @@
+import React from "react";
+import eraser from "../../assets/drawingTools/eraser.png";
+import redPencil from "../../assets/drawingTools/colorPencil_red.png";
+import orangePencil from "../../assets/drawingTools/colorPencil_orange.png";
+import yellowPencil from "../../assets/drawingTools/colorPencil_yellow.png";
+import greenPencil from "../../assets/drawingTools/colorPencil_green.png";
+import bluePencil from "../../assets/drawingTools/colorPencil_blue.png";
+import navyPencil from "../../assets/drawingTools/colorPencil_navy.png";
+import purplePencil from "../../assets/drawingTools/colorPencil_purple.png";
+import pinkPencil from "../../assets/drawingTools/colorPencil_pink.png";
+import rainbowPencil from "../../assets/drawingTools/colorPencil_rainbow.png";
+
+const DrawingTools = () => {
+  return (
+    <>
+      <ToolSlide />
+    </>
+  );
+};
+
+export default DrawingTools;
+
+const ToolSlide = () => {
+  const toolDatas = [
+    eraser,
+    redPencil,
+    orangePencil,
+    yellowPencil,
+    greenPencil,
+    bluePencil,
+    navyPencil,
+    purplePencil,
+    pinkPencil,
+    rainbowPencil,
+  ];
+  return (
+    <div className="flex fixed bottom-0 overflow-x-scroll">
+      {toolDatas.map((tool) => (
+        <img src={tool} />
+      ))}
+    </div>
+  );
+};
