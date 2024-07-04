c02af38f8c39d6da49421f4f0e45953ecb782122
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-07-04 13:10:05 +0900

    [refactor] BottomSheet 안 CalenderDetail 넣어보기

--- src/pages/test/TestBottomSheetTutorialPage.jsx
+++ src/pages/test/TestBottomSheetTutorialPage.jsx
@@ -1,5 +1,11 @@
 import { useState } from "react";
-import { BOTTOM_POSITION, BottomSheetLayout, MIDDLE_POSITION, TOP_POSITION } from "../../layouts/BottomSheetLayout";
+import {
+  BOTTOM_POSITION,
+  BottomSheetLayout,
+  MIDDLE_POSITION,
+  TOP_POSITION,
+} from "../../layouts/BottomSheetLayout";
+import { CalendarDetail } from "../../components/calendar_detail/CalendarDetail";
 
 export const TEST_BOTTOM_SHEET_PAGE_PATH = "/test/bottom/sheet/diary";
 
@@ -8,19 +14,33 @@ export const TestBottomSheetTutorialPage = () => {
 
   return (
     <>
-      <div onClick={() => {
-        setPosition(TOP_POSITION);
-      }}>{"맨위로"}</div>
-      <div onClick={() => {
-        setPosition(MIDDLE_POSITION);
-      }}>{"중간으로"}</div>
-      <div onClick={() => {
-        setPosition(BOTTOM_POSITION);
-      }}>{"닫기"}</div>
-      <div className={"w-full h-[300px] bg-blue-600"}>여기 대충 달력</div>
+      <div
+        onClick={() => {
+          setPosition(TOP_POSITION);
+        }}
+      >
+        {"맨위로"}
+      </div>
+      <div
+        onClick={() => {
+          setPosition(MIDDLE_POSITION);
+        }}
+      >
+        {"중간으로"}
+      </div>
+      <div
+        onClick={() => {
+          setPosition(BOTTOM_POSITION);
+        }}
+      >
+        {"닫기"}
+      </div>
+      <div className={"w-full h-[300px] bg-blue-600 rounded-b-2xl"}>
+        여기 대충 달력
+      </div>
 
       <BottomSheetLayout position={position} setPosition={setPosition}>
-        <div className={"h-full bg-primary-600"}>this</div>
+        <CalendarDetail />
       </BottomSheetLayout>
     </>
   );
