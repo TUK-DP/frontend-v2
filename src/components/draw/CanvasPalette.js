a4beb259957d4dcef80670f795212758cf99cc66
Hj1218 <wjdguswl2341@naver.com>
2024-07-05 23:33:48 +0900

    [publishing] 토글로 팔레트 열고 닫기 추가

--- src/components/draw/CanvasPalette.js
+++ src/components/draw/CanvasPalette.js
@@ -1,25 +1,59 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import DrawingToolBar from "./DrawingToolBar";
-import { TbTriangleFilled } from "react-icons/tb";
+import { TbTriangleFilled, TbTriangleInvertedFilled } from "react-icons/tb";
 
 const CanvasPalette = () => {
-  const [selectedColor, setSelectedColor] = useState("transparent"); //초기 색상 설정
+  const [selectedColor, setSelectedColor] = useState("transparent"); // 초기 색상 설정
+  const paletteRef = useRef(null); // useRef로 팔레트의 DOM 요소를 저장할 변수 생성
+  const [isOpen, setIsOpen] = useState(true);
+  const [paletteHeight, setPaletteHeight] = useState(0); // 팔레트의 높이를 상태로 관리
+
+  // useEffect를 이용하여 팔레트의 높이를 측정하고 상태에 저장
+  useEffect(() => {
+    if (paletteRef.current) {
+      setPaletteHeight(paletteRef.current.clientHeight);
+    }
+  }, [paletteRef.current]); // 팔레트 ref가 변경될 때마다 실행
+
+  const togglePalette = () => {
+    setIsOpen((prevOpen) => !prevOpen);
+  };
 
   return (
-    <div className="fixed bottom-[-7rem]">
-      <div className="bg-[#d9d9d9] flex justify-center items-center py-2 rounded-ss-2xl rounded-se-2xl">
-        <TbTriangleFilled />
+    <div className="fixed bottom-0 right-0">
+      <div
+        className="transition-transform duration-500"
+        style={{
+          transform: isOpen
+            ? `translateY(9rem)`
+            : `translateY(${paletteHeight}px)`,
+        }}
+      >
+        <PaletteToggle isOpen={isOpen} togglePalette={togglePalette} />
+        <div ref={paletteRef} className={`palette ${isOpen ? "open" : ""}`}>
+          <BrushSizeControl selectedColor={selectedColor} />
+          <DrawingToolBar setSelectedColor={setSelectedColor} />
+        </div>
       </div>
-      <BrushSizeControl selectedColor={selectedColor} />
-      <DrawingToolBar setSelectedColor={setSelectedColor} />
     </div>
   );
 };
 
 export default CanvasPalette;
 
+const PaletteToggle = ({ isOpen, togglePalette }) => {
+  return (
+    <div
+      onClick={togglePalette}
+      className="bg-[#d9d9d9] flex justify-center items-center py-2 rounded-ss-2xl rounded-se-2xl cursor-pointer"
+    >
+      {isOpen ? <TbTriangleInvertedFilled /> : <TbTriangleFilled />}
+    </div>
+  );
+};
+
 const BrushSizeControl = ({ selectedColor }) => {
-  const [circleSize, setCircleSize] = useState(20); //초기 원의 크기 설정
+  const [circleSize, setCircleSize] = useState(20); // 초기 원의 크기 설정
   return (
     <div className="flex items-center mx-3">
       <input
