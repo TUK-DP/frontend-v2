c816ba7cae0d1db7f74a0a393e8ae666289f8886
Hj1218 <wjdguswl2341@naver.com>
2024-07-05 20:25:19 +0900

    [publishing] range, 크기 조정 보여주는 원 추가

--- src/components/draw/CanvasPalette.js
+++ src/components/draw/CanvasPalette.js
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import DrawingToolBar from "./DrawingToolBar";
 
 const CanvasPalette = () => {
+  const [circleSize, setCircleSize] = useState(20); // 초기 원의 크기 설정
+
+  // range input 값이 변경될 때 호출되는 함수
+  const handleRangeChange = (event) => {
+    setCircleSize(parseInt(event.target.value));
+  };
+
   return (
     <div className="fixed bottom-[-7rem]">
-      <input type="range" />
+      <div className="flex items-center mx-3">
+        <input
+          type="range"
+          min="20"
+          max="50"
+          value={circleSize}
+          onChange={handleRangeChange}
+          className="mr-4 flex-1"
+        />
+        <div className="w-16 h-16 flex justify-center items-center">
+          <div
+            className="bg-red-500 rounded-full"
+            style={{
+              width: `${circleSize}px`,
+              height: `${circleSize}px`,
+            }}
+          />
+        </div>
+      </div>
       <DrawingToolBar />
     </div>
   );
