25e0f61f17c853625d87a8b80205e1af1f45f62a
Hj1218 <wjdguswl2341@naver.com>
2024-07-05 20:40:33 +0900

    [publishing] BrushSizeControl 컴포넌트 분리

--- src/components/draw/CanvasPalette.js
+++ src/components/draw/CanvasPalette.js
@@ -2,34 +2,43 @@ import React, { useState } from "react";
 import DrawingToolBar from "./DrawingToolBar";
 
 const CanvasPalette = () => {
-  const [circleSize, setCircleSize] = useState(20); // 초기 원의 크기 설정
   const [selectedColor, setSelectedColor] = useState("transparent"); // 초기 색상 설정
 
   return (
     <div className="fixed bottom-[-7rem]">
-      <div className="flex items-center mx-3">
-        <input
-          type="range"
-          min="20"
-          max="50"
-          value={circleSize}
-          onChange={(event) => setCircleSize(parseInt(event.target.value))}
-          className="mr-4 flex-1"
-        />
-        <div className="w-16 h-16 flex justify-center items-center">
-          <div
-            className="rounded-full"
-            style={{
-              width: `${circleSize}px`,
-              height: `${circleSize}px`,
-              backgroundColor: selectedColor,
-            }}
-          />
-        </div>
-      </div>
+      <BrushSizeControl
+        selectedColor={selectedColor}
+        setSelectedColor={setSelectedColor}
+      />
       <DrawingToolBar setSelectedColor={setSelectedColor} />
     </div>
   );
 };
 
 export default CanvasPalette;
+
+const BrushSizeControl = ({ selectedColor }) => {
+  const [circleSize, setCircleSize] = useState(20); // 초기 원의 크기 설정
+  return (
+    <div className="flex items-center mx-3">
+      <input
+        type="range"
+        min="20"
+        max="50"
+        value={circleSize}
+        onChange={(event) => setCircleSize(parseInt(event.target.value))}
+        className="mr-4 flex-1"
+      />
+      <div className="w-16 h-16 flex justify-center items-center">
+        <div
+          className={`rounded-full ${selectedColor === "transparent" ? "border-4" : ""}`}
+          style={{
+            width: `${circleSize}px`,
+            height: `${circleSize}px`,
+            backgroundColor: selectedColor,
+          }}
+        />
+      </div>
+    </div>
+  );
+};
