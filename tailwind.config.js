8d5126ad07ba66e35ed373680086c97f2a49d66a
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-07-06 12:54:03 +0900

    [fix] tailwind.config.js 파일 오류 수정

--- tailwind.config.js
+++ tailwind.config.js
@@ -23,10 +23,11 @@ module.exports = {
         mobile: { min: "240px", max: "640px" },
         tablet: { min: "641px", max: "1280" },
         pc: { min: "1280" },
-      borderRadius: {
-        "lg-xl": "0.625rem", //10px
+        borderRadius: {
+          "lg-xl": "0.625rem", //10px
+        },
       },
     },
+    plugins: [],
   },
-  plugins: [],
 };
