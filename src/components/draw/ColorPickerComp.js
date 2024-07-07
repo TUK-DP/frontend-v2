7651cc8bc40e5db9585d20e18600425e592f8113
Hj1218 <wjdguswl2341@naver.com>
2024-07-07 18:57:24 +0900

    [publishing] 코드 중복 제거

--- src/components/draw/ColorPickerComp.js
+++ src/components/draw/ColorPickerComp.js
@@ -1,22 +1,16 @@
 import React from "react";
-import { ColorPicker } from "react-color-palette";
+import { ColorPicker, useColor } from "react-color-palette";
+import "react-color-palette/css";
 
-const ColorPickerComp = ({
-  color,
-  setColor,
-  setSelectedColor,
-  setColorPickerOpen,
-}) => {
-  const handleChangeColor = (newColor) => {
-    setColor(newColor);
-  };
+const ColorPickerComp = ({ setSelectedColor, setColorPickerOpen }) => {
+  const [color, setColor] = useColor("hex", "#FF0900");
 
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50">
       <div className="bg-white p-4 rounded-md w-[20rem] md:w-[35rem]">
         <ColorPicker
           color={color}
-          onChange={handleChangeColor}
+          onChange={setColor}
           hideInput={["rgb", "hsv", "hex"]} // 옵션 숨기기
         />
         <div className="flex justify-end mt-4">
