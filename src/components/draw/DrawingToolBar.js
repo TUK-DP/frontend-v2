9431310fb5b5e51c12d395afa479d402f79a0d46
Hj1218 <wjdguswl2341@naver.com>
2024-07-08 22:14:10 +0900

    [publishing] 그리기 도구 추가

--- src/components/draw/DrawingToolBar.js
+++ src/components/draw/DrawingToolBar.js
@@ -1,13 +1,21 @@
 import React, { useState } from "react";
 import eraser from "../../assets/drawingTools/eraser.png";
+import blackPencil from "../../assets/drawingTools/colorPencil_black.png";
 import redPencil from "../../assets/drawingTools/colorPencil_red.png";
 import orangePencil from "../../assets/drawingTools/colorPencil_orange.png";
+import coralPencil from "../../assets/drawingTools/colorPencil_coral.png";
 import yellowPencil from "../../assets/drawingTools/colorPencil_yellow.png";
 import greenPencil from "../../assets/drawingTools/colorPencil_green.png";
+import olivePencil from "../../assets/drawingTools/colorPencil_olive.png";
 import bluePencil from "../../assets/drawingTools/colorPencil_blue.png";
 import navyPencil from "../../assets/drawingTools/colorPencil_navy.png";
 import purplePencil from "../../assets/drawingTools/colorPencil_purple.png";
+import plumPencil from "../../assets/drawingTools/colorPencil_plum.png";
 import pinkPencil from "../../assets/drawingTools/colorPencil_pink.png";
+import babypinkPencil from "../../assets/drawingTools/colorPencil_babypink.png";
+import apricotPencil from "../../assets/drawingTools/colorPencil_apricot.png";
+import brownPencil from "../../assets/drawingTools/colorPencil_brown.png";
+import grayPencil from "../../assets/drawingTools/colorPencil_gray.png";
 import rainbowPencil from "../../assets/drawingTools/colorPencil_rainbow.png";
 
 const DrawingToolBar = ({ setSelectedColor, setColorPickerOpen }) => {
@@ -15,22 +23,22 @@ const DrawingToolBar = ({ setSelectedColor, setColorPickerOpen }) => {
 
   const toolDatas = [
     { tool: eraser, color: "transparent" },
+    { tool: blackPencil, color: "#000000" },
     { tool: redPencil, color: "#FF0900" },
     { tool: orangePencil, color: "#FF7700" },
+    { tool: coralPencil, color: "#FF9B8A" },
     { tool: yellowPencil, color: "#FFF200" },
     { tool: greenPencil, color: "#88FF00" },
+    { tool: olivePencil, color: "#007A08" },
     { tool: bluePencil, color: "#0051FF" },
     { tool: navyPencil, color: "#0C00AD" },
     { tool: purplePencil, color: "#9000FF" },
+    { tool: plumPencil, color: "#78007A" },
     { tool: pinkPencil, color: "#FF00EA" },
-    { tool: redPencil, color: "#FF0900" },
-    { tool: orangePencil, color: "#FF7700" },
-    { tool: yellowPencil, color: "#FFF200" },
-    { tool: greenPencil, color: "#88FF00" },
-    { tool: bluePencil, color: "#0051FF" },
-    { tool: navyPencil, color: "#0C00AD" },
-    { tool: purplePencil, color: "#9000FF" },
-    { tool: pinkPencil, color: "#FF00EA" },
+    { tool: babypinkPencil, color: "#FFA6E6" },
+    { tool: apricotPencil, color: "#FFE4CF" },
+    { tool: brownPencil, color: "#854A19" },
+    { tool: grayPencil, color: "#919191" },
     {
       tool: rainbowPencil,
     },
