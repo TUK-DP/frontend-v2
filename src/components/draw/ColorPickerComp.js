e957b5ce707376389ce358c2d188ffec8d2dd444
Hj1218 <wjdguswl2341@naver.com>
2024-07-10 23:10:03 +0900

    [publishing] 색상 선택 문구 추가, 버튼 색상 변경

--- src/components/draw/ColorPickerComp.js
+++ src/components/draw/ColorPickerComp.js
@@ -11,6 +11,9 @@ const ColorPickerComp = ({
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50">
       <div className="bg-white p-6 rounded-lg shadow-lg w-[20rem] md:w-[35rem]">
+        <div className="w-full font-bold text-xl md:text-3xl flex justify-center mb-3">
+          원하는 색상을 선택하세요
+        </div>
         <ColorPicker
           color={color}
           onChange={setColor}
@@ -19,7 +22,7 @@ const ColorPickerComp = ({
         <div className="flex items-center justify-between mt-6">
           <ColorState color={color} />
           <button
-            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded-md shadow-md transition duration-300"
+            className="bg-[#6100C1] hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded-md shadow-md transition duration-300"
             onClick={() => {
               setSelectedColor(color.hex);
               setColorPickerOpen(false);
@@ -38,7 +41,7 @@ export default ColorPickerComp;
 const ColorState = ({ color }) => {
   return (
     <div className="flex items-center">
-      <strong className="md:text-xl">선택한 색상:</strong>
+      <strong className="md:text-2xl">선택한 색상:</strong>
       <div
         className="w-20 md:w-28 h-8 border rounded-md ml-2 md:ml-5 shadow-inner"
         style={{ backgroundColor: color.hex }}
