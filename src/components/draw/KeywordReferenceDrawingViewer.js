37e89b3beff803990102c55ba7592d8427deb158
Hj1218 <wjdguswl2341@naver.com>
2024-07-11 00:50:56 +0900

    [publishing] 리스트에서 선택한 그림(사진) 띄우기

--- src/components/draw/KeywordReferenceDrawingViewer.js
+++ src/components/draw/KeywordReferenceDrawingViewer.js
@@ -1,20 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
+import img1 from "../../assets/remembrance/noKeyword.png";
+import img2 from "../../assets/remembrance/questionMark.png";
 
 const KeywordReferenceDrawingViewer = () => {
+  const [selectedDrawing, setSelectedDrawing] = useState({
+    id: 1,
+    src: img1,
+  });
+
   return (
-    <div>
-      <SelectedDrawingViewer />
-      <DrawingListScroller />
-    </div>
+    <>
+      <SelectedDrawingViewer selectedDrawing={selectedDrawing} />
+      <DrawingListScroller setSelectedDrawing={setSelectedDrawing} />
+    </>
   );
 };
 
 export default KeywordReferenceDrawingViewer;
 
-const SelectedDrawingViewer = () => {
-  return <></>;
+const SelectedDrawingViewer = ({ selectedDrawing }) => {
+  return (
+    <div className="p-8 md:p-16">
+      <div className="w-full aspect-square border-2 border-black rounded-xl overflow-hidden">
+        <img
+          src={selectedDrawing.src}
+          alt="Selected Drawing"
+          className="w-full h-full object-cover"
+        />
+      </div>
+    </div>
+  );
 };
 
-const DrawingListScroller = () => {
-  return <></>;
+const DrawingListScroller = ({ setSelectedDrawing }) => {
+  const DrawingList = [
+    { id: 1, src: img1 },
+    { id: 2, src: img2 },
+    { id: 3, src: img1 },
+    { id: 4, src: img2 },
+    { id: 5, src: img1 },
+    { id: 6, src: img2 },
+    { id: 7, src: img1 },
+    { id: 8, src: img2 },
+    { id: 9, src: img1 },
+    { id: 10, src: img2 },
+  ];
+
+  return (
+    <div className="flex overflow-x-scroll scrollbar-hide">
+      {DrawingList.map((drawing) => (
+        <img
+          key={drawing.id}
+          src={drawing.src}
+          className="w-20 md:w-32 aspect-square ml-6 md:ml-10 border-2 border-black cursor-pointer"
+          onClick={() => setSelectedDrawing(drawing)}
+        />
+      ))}
+    </div>
+  );
 };
