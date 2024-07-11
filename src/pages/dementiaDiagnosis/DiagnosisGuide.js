32e07d0db7d8eadda6beb89046f392ace1e3db6a
Hj1218 <wjdguswl2341@naver.com>
2024-07-12 00:14:21 +0900

    [publishing] 치매진단 안내페이지 생성, 경로추가

---
+++ src/pages/dementiaDiagnosis/DiagnosisGuide.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const DIAGNOSIS_GUIDE_PAGE_PATH = "/diagnosisguide";
+
+const DiagnosisGuide = () => {
+  return <div>DiagnosisGuide</div>;
+};
+
+export default DiagnosisGuide;
