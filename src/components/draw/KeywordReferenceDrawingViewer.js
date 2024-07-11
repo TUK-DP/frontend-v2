1f27bfc76c19396bb147fb69a851e344e61010d5
Hj1218 <wjdguswl2341@naver.com>
2024-07-11 21:32:58 +0900

    [publishing] 선택된 그림(사진) 중앙으로 위치 이동 추가

--- src/components/draw/KeywordReferenceDrawingViewer.js
+++ src/components/draw/KeywordReferenceDrawingViewer.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import img1 from "../../assets/remembrance/noKeyword.png";
 import img2 from "../../assets/remembrance/questionMark.png";
 
@@ -49,14 +49,46 @@ const DrawingListScroller = ({ selectedDrawing, setSelectedDrawing }) => {
     { id: 10, src: img2 },
   ];
 
+  const scrollerRef = useRef(null);
+
+  useEffect(() => {
+    if (scrollerRef.current) {
+      //선택된 그림 요소 찾기
+      const selectedElement = scrollerRef.current.querySelector(
+        `img[data-id='${selectedDrawing.id}']`
+      );
+      if (selectedElement) {
+        const containerWidth = scrollerRef.current.offsetWidth; //컨테이너 너비
+        const selectedElementWidth = selectedElement.offsetWidth; //선택된 그림 너비
+        // 선택된 그림의 offsetLeft - (컨테이너 너비)/2 + 선택된 그림 너비 절반 = 컨테이너의 중앙
+        const scrollPosition =
+          selectedElement.offsetLeft -
+          containerWidth / 2 +
+          selectedElementWidth / 2;
+        scrollerRef.current.scrollTo({
+          left: scrollPosition,
+          behavior: "smooth",
+        });
+      }
+    }
+  }, [selectedDrawing]);
+
   return (
-    <div className="flex overflow-x-scroll scrollbar-hide pt-10">
+    <div
+      ref={scrollerRef}
+      className="flex overflow-x-scroll scrollbar-hide pt-10"
+    >
       {DrawingList.map((drawing) => (
         <img
           key={drawing.id}
+          data-id={drawing.id}
           src={drawing.src}
           className={`transition-transform duration-500 w-20 md:w-32 aspect-square ml-6 md:ml-10 cursor-pointer
-            ${selectedDrawing.id === drawing.id ? "-translate-y-10 border-2 border-[#6100C1]" : "border border-black"}`}
+            ${
+              selectedDrawing.id === drawing.id
+                ? "-translate-y-10 border-2 border-[#6100C1]"
+                : "border border-black"
+            }`}
           onClick={() => setSelectedDrawing(drawing)}
         />
       ))}
