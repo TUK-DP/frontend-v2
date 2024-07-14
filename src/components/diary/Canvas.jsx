75054cac0965dc7f04caf6597403ea312b13afb8
heedong12 <dlgmldus3289@naver.com>
2024-07-14 12:13:48 +0900

    [publishing] 에러메시지 추가

--- src/components/diary/Canvas.jsx
+++ src/components/diary/Canvas.jsx
@@ -5,8 +5,10 @@ import { FaRegCircleCheck } from "react-icons/fa6";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Spinner from "../Spinner";
 
 const Canvas = ({ keywords, setCanvasSlider, canvasSlider }) => {
+  const [isError, setIsError] = useState(false);
   return (
     <>
       <CanvasSlider
@@ -14,7 +16,8 @@ const Canvas = ({ keywords, setCanvasSlider, canvasSlider }) => {
         setCanvasSlider={setCanvasSlider}
         canvasSlider={canvasSlider}
       />
-      <CanvasTools />
+      <CanvasTools setIsError={setIsError} />
+      <ErrorMessage isError={isError} />
     </>
   );
 };
@@ -50,7 +53,7 @@ const CanvasSlider = ({ keywords, setCanvasSlider, keywordSlider }) => {
   );
 };
 
-const CanvasTools = () => {
+const CanvasTools = ({ setIsError }) => {
   return (
     <div className={"flex flex-row gap-10 justify-evenly items-center pt-10"}>
       <HiMiniArrowUturnLeft
@@ -58,7 +61,7 @@ const CanvasTools = () => {
         color="#838383"
         className={"cursor-pointer"}
       />
-      <CompleteButton />
+      <CompleteButton setIsError={setIsError} />
       <HiMiniArrowUturnRight
         size={44}
         color="#838383"
@@ -68,15 +71,45 @@ const CanvasTools = () => {
   );
 };
 
-const CompleteButton = () => {
+const CompleteButton = ({ setIsError }) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const handleClickCompleteButton = () => {
+    //완료버튼 누르면 스피너 돌아가감
+    //오류가 뜨면 오류메세지 출력
+    //오류 없으면 페이지 이동
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsError(true);
+      setIsLoading(false);
+    }, 3000);
+  };
   return (
     <button
       className={
         "mobile:text-2xl text-4xl font-bold text-white bg-primary-600 mobile:w-28 mobile:h-11 w-40 h-16 rounded-3.5xl flex flex-row justify-center items-center gap-2"
       }
+      onClick={handleClickCompleteButton}
     >
-      <p>완료</p>
-      <FaRegCircleCheck className={"mobile:w-7 w-12"} color="#ffffff" />
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <>
+          <p>완료</p>
+          <FaRegCircleCheck className={"mobile:w-7 w-12"} color="#ffffff" />
+        </>
+      )}
     </button>
+  );
+};
+
+const ErrorMessage = ({ isError }) => {
+  return (
+    <div
+      className={
+        "w-full grid place-items-center text-[#FF0000] pt-3  font-bold mobile:text-lg text-3xl"
+      }
+    >
+      {isError && "오류입니다. 잠시 후 다시 저장해주세요."}
+    </div>
   );
 };
