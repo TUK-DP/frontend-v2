c2eae7043e4ca1fe28543d6b05534142dad31f74
heedong12 <dlgmldus3289@naver.com>
2024-07-14 12:10:46 +0900

    [publishing] 반응형으로 수정

--- src/components/diary/Canvas.jsx
+++ src/components/diary/Canvas.jsx
@@ -8,16 +8,24 @@ const Canvas = () => {
     <div>
       <canvas className={"w-full bg-white aspect-square"}></canvas>
       <div className={"flex flex-row gap-10 justify-evenly items-center pt-10"}>
-        <HiMiniArrowUturnLeft size={44} color="#838383" />
+        <HiMiniArrowUturnLeft
+          size={44}
+          color="#838383"
+          className={"cursor-pointer"}
+        />
         <button
           className={
-            "text-2xl font-bold text-white bg-primary-600 w-28 h-11 rounded-3.5xl flex flex-row justify-center items-center gap-2"
+            "mobile:text-2xl text-4xl font-bold text-white bg-primary-600 mobile:w-28 mobile:h-11 w-40 h-16 rounded-3.5xl flex flex-row justify-center items-center gap-2"
           }
         >
           <p>완료</p>
-          <FaRegCircleCheck size={28} color="#ffffff" />
+          <FaRegCircleCheck className={"mobile:w-7 w-12"} color="#ffffff" />
         </button>
-        <HiMiniArrowUturnRight size={44} color="#838383" />
+        <HiMiniArrowUturnRight
+          size={44}
+          color="#838383"
+          className={"cursor-pointer"}
+        />
       </div>
     </div>
   );
--- src/pages/DiaryDraw.jsx
+++ src/pages/DiaryDraw.jsx
@@ -5,7 +5,6 @@ import KeywordSlider from "../components/diary/KeywordSlider";
 import Canvas from "../components/diary/Canvas";
 import aiHelpRobot from "../assets/draw/aiHelpRobot.png";
 import showOtherDraw from "../assets/draw/showOtherDraw.png";
-import { createJSONStorage } from "zustand/middleware";
 
 export const DIARY_DRAW_PAGE_PATH = "/diary/draw";
 const DiaryDraw = () => {
@@ -13,7 +12,7 @@ const DiaryDraw = () => {
   return (
     <PaperBackgroundWrapper>
       <HeaderBar />
-      <div className={"px-5"}>
+      <div className={"mobile:px-5 px-20 pb-5"}>
         <KeywordSlider keywords={keywords} />
         <Canvas />
         <DrawHelpWrapper>
@@ -29,7 +28,9 @@ export default DiaryDraw;
 const DrawHelpWrapper = ({ children }) => {
   return (
     <div>
-      <div className={"font-bold text-2xl py-5"}>혹시 그리기 어려우신가요?</div>
+      <div className={"font-bold mobile:text-2xl mobile:py-5 text-4xl py-10 "}>
+        혹시 그리기 어려우신가요?
+      </div>
       {children}
     </div>
   );
@@ -39,23 +40,25 @@ const DrawHelpButtons = () => {
     <div className={"flex gap-5 justify-center items-center"}>
       <div
         className={
-          "w-1/2 aspect-square bg-aiHelpButton rounded-lg-xl p-5 flex flex-col justify-between items-end"
+          "w-1/2 aspect-square bg-aiHelpButton rounded-lg-xl flex flex-col justify-between items-end mobile:p-5 p-10 cursor-pointer"
         }
       >
-        <p className={"font-bold text-xl text-left w-full"}>AI 도움받기</p>
-        <img src={aiHelpRobot} className={"w-16"} />
+        <p className={"font-bold mobile:text-xl text-4xl text-left w-full"}>
+          AI 도움받기
+        </p>
+        <img src={aiHelpRobot} className={"mobile:w-16 w-32"} />
       </div>
       <div
         className={
-          "w-1/2 aspect-square bg-showOtherDrawButton rounded-lg-xl px-5 pt-2 pb-5 flex flex-col justify-between items-end"
+          "w-1/2 aspect-square bg-showOtherDrawButton rounded-lg-xl flex flex-col justify-between items-end mobile:px-5 mobile:p-5 p-10 cursor-pointer"
         }
       >
-        <p className={"font-bold text-xl text-left w-full"}>
+        <p className={"font-bold mobile:text-xl text-4xl text-left w-full"}>
           다른 사람
           <br />
           그림 구경하기
         </p>
-        <img src={showOtherDraw} className={"w-16"} />
+        <img src={showOtherDraw} className={"mobile:w-16 w-32"} />
       </div>
     </div>
   );
