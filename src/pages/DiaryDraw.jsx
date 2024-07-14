9bc1d8a017c688d935152a1663d8e77020b3910f
heedong12 <dlgmldus3289@naver.com>
2024-07-14 12:13:05 +0900

    [publishing] 버튼 컴포넌트화

--- src/pages/DiaryDraw.jsx
+++ src/pages/DiaryDraw.jsx
@@ -46,31 +46,37 @@ const DrawHelpWrapper = ({ children }) => {
     </div>
   );
 };
+
 const DrawHelpButtons = () => {
   return (
     <div className={"flex gap-5 justify-center items-center"}>
-      <div
+      <DrawHelpButton
+        content={"AI 도움받기"}
+        imgUrl={aiHelpRobot}
+        bgColor="aiHelpButton"
+      />
+      <DrawHelpButton
+        content={["다른 사람\n그림 구경하기"]}
+        imgUrl={showOtherDraw}
+        bgColor="showOtherDrawButton"
+      />
+    </div>
+  );
+};
+
+const DrawHelpButton = ({ content, imgUrl, bgColor }) => {
+  return (
+    <div
+      className={`w-1/2 aspect-square bg-${bgColor} rounded-lg-xl flex flex-col justify-between items-end mobile:p-5 p-10 cursor-pointer`}
+    >
+      <p
         className={
-          "w-1/2 aspect-square bg-aiHelpButton rounded-lg-xl flex flex-col justify-between items-end mobile:p-5 p-10 cursor-pointer"
+          "font-bold mobile:text-xl text-4xl text-left w-full whitespace-pre-wrap"
         }
       >
-        <p className={"font-bold mobile:text-xl text-4xl text-left w-full"}>
-          AI 도움받기
-        </p>
-        <img src={aiHelpRobot} className={"mobile:w-16 w-32"} />
-      </div>
-      <div
-        className={
-          "w-1/2 aspect-square bg-showOtherDrawButton rounded-lg-xl flex flex-col justify-between items-end mobile:px-5 mobile:p-5 p-10 cursor-pointer"
-        }
-      >
-        <p className={"font-bold mobile:text-xl text-4xl text-left w-full"}>
-          다른 사람
-          <br />
-          그림 구경하기
-        </p>
-        <img src={showOtherDraw} className={"mobile:w-16 w-32"} />
-      </div>
+        {content}
+      </p>
+      <img src={imgUrl} className={"mobile:w-16 w-32"} />
     </div>
   );
 };
