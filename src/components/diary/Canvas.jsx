90bdf9540a81994ce386f46d33506f01bcbd492f
heedong12 <dlgmldus3289@naver.com>
2024-07-14 12:13:28 +0900

    [refactor]컴포넌트 작게 쪼갬

--- src/components/diary/Canvas.jsx
+++ src/components/diary/Canvas.jsx
@@ -8,33 +8,14 @@ import "slick-carousel/slick/slick-theme.css";
 
 const Canvas = ({ keywords, setCanvasSlider, canvasSlider }) => {
   return (
-    <div>
+    <>
       <CanvasSlider
         keywords={keywords}
         setCanvasSlider={setCanvasSlider}
         canvasSlider={canvasSlider}
       />
-      <div className={"flex flex-row gap-10 justify-evenly items-center pt-10"}>
-        <HiMiniArrowUturnLeft
-          size={44}
-          color="#838383"
-          className={"cursor-pointer"}
-        />
-        <button
-          className={
-            "mobile:text-2xl text-4xl font-bold text-white bg-primary-600 mobile:w-28 mobile:h-11 w-40 h-16 rounded-3.5xl flex flex-row justify-center items-center gap-2"
-          }
-        >
-          <p>완료</p>
-          <FaRegCircleCheck className={"mobile:w-7 w-12"} color="#ffffff" />
-        </button>
-        <HiMiniArrowUturnRight
-          size={44}
-          color="#838383"
-          className={"cursor-pointer"}
-        />
-      </div>
-    </div>
+      <CanvasTools />
+    </>
   );
 };
 
@@ -66,5 +47,36 @@ const CanvasSlider = ({ keywords, setCanvasSlider, keywordSlider }) => {
         ></canvas>
       ))}
     </Slider>
+  );
+};
+
+const CanvasTools = () => {
+  return (
+    <div className={"flex flex-row gap-10 justify-evenly items-center pt-10"}>
+      <HiMiniArrowUturnLeft
+        size={44}
+        color="#838383"
+        className={"cursor-pointer"}
+      />
+      <CompleteButton />
+      <HiMiniArrowUturnRight
+        size={44}
+        color="#838383"
+        className={"cursor-pointer"}
+      />
+    </div>
+  );
+};
+
+const CompleteButton = () => {
+  return (
+    <button
+      className={
+        "mobile:text-2xl text-4xl font-bold text-white bg-primary-600 mobile:w-28 mobile:h-11 w-40 h-16 rounded-3.5xl flex flex-row justify-center items-center gap-2"
+      }
+    >
+      <p>완료</p>
+      <FaRegCircleCheck className={"mobile:w-7 w-12"} color="#ffffff" />
+    </button>
   );
 };
