5e2e406a94fa25db778e336a07eee55803e305eb
heedong12 <dlgmldus3289@naver.com>
2024-07-15 18:40:46 +0900

    [publishing]일기회상 문제 있을 때 페이지

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,32 @@ import { TestApp } from "./pages/test/TestApp";
 import { MobileResponsiveLayout } from "./layouts/MobileResponsiveLayout";
 import Signup, { SIGNUP_PAGE_PATH } from "./pages/Signup";
 import Signin, { SIGNIN_PAGE_PATH } from "./pages/Signin";
-import { TEST_BOTTOM_SHEET_PAGE_PATH, TestBottomSheetTutorialPage } from "./pages/test/TestBottomSheetTutorialPage";
-
+import {
+  TEST_BOTTOM_SHEET_PAGE_PATH,
+  TestBottomSheetTutorialPage,
+} from "./pages/test/TestBottomSheetTutorialPage";
+import DiaryRecall, { DIARY_RECALL_PAGE_PATH } from "./pages/DiaryRecall";
 
 function App() {
   return (
- <MobileResponsiveLayout>
+    <MobileResponsiveLayout>
       <BrowserRouter>
         <Routes>
           <Route exact element={<NavigationBarLayout />}>
             <Route exact path={"/"} element={<TestApp />} />
-            <Route exact path={TEST_BOTTOM_SHEET_PAGE_PATH} element={<TestBottomSheetTutorialPage />} />
+            <Route
+              exact
+              path={TEST_BOTTOM_SHEET_PAGE_PATH}
+              element={<TestBottomSheetTutorialPage />}
+            />
           </Route>
           <Route path={SIGNUP_PAGE_PATH} element={<Signup />} />
           <Route path={SIGNIN_PAGE_PATH} element={<Signin />} />
+          <Route path={DIARY_RECALL_PAGE_PATH} element={<DiaryRecall />} />
         </Routes>
       </BrowserRouter>
     </MobileResponsiveLayout>
-  );  
+  );
 }
 
 export default App;
---
+++ src/pages/DiaryRecall.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import DiaryRecallImg from "../assets/diary/questionPeople.png";
+
+export const DIARY_RECALL_PAGE_PATH = "/diary/recall";
+const DiaryRecall = () => {
+  return (
+    <div
+      className={
+        "flex flex-col px-5  items-center gap-5 text-center py-20 font-bold"
+      }
+    >
+      <div className={"text-4xl"}>
+        일기를 회상해서
+        <br />
+        기억을 되살려 보세요!
+      </div>
+      <div className={"text-2xl text-secondary-500"}>
+        일기의 빈칸을 떠올려 보세요
+      </div>
+      <img src={DiaryRecallImg} alt="diary-recall" />
+      <div className={"font-semibold text-left flex flex-col gap-5 py-5"}>
+        <div>
+          각 빈칸에 적었던 단어를 입력해
+          <br />
+          문제를 풀어보세요
+        </div>
+        <div>
+          해당 키워드는 TextRank 알고리즘을
+          <br />
+          이용해 추출한 키워드입니다.
+        </div>
+        <p className={"text-secondary-700"}>
+          영어로 된 키워드는 현재 뽑히지 않습니다.
+        </p>
+      </div>
+      <button className={"text-xl border-2 w-4/5 h-12 rounded-xl"}>
+        문제 풀기
+      </button>
+    </div>
+  );
+};
+
+export default DiaryRecall;
