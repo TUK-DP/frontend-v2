5f37057b019107eab2f3875916d27430160b4351
정현지 <114800998+Hj1218@users.noreply.github.com>
2024-07-15 21:55:08 +0900

    Publishing/28 치매진단 시작 전 안내페이지 퍼블리싱 (#29)
    * [publishing] 치매진단 안내페이지 생성, 경로추가
    
    * [publishing] 버튼 컴포넌트 생성
    
    * [publishing] 치매진단 안내 페이지 퍼블리싱
    
    * [publishing] 단어 단위로 잘리도록 break-keep 추가

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,37 @@ import { TestApp } from "./pages/test/TestApp";
 import { MobileResponsiveLayout } from "./layouts/MobileResponsiveLayout";
 import Signup, { SIGNUP_PAGE_PATH } from "./pages/Signup";
 import Signin, { SIGNIN_PAGE_PATH } from "./pages/Signin";
-import { TEST_BOTTOM_SHEET_PAGE_PATH, TestBottomSheetTutorialPage } from "./pages/test/TestBottomSheetTutorialPage";
-
+import DiagnosisGuide, {
+  DIAGNOSIS_GUIDE_PAGE_PATH,
+} from "./pages/dementiaDiagnosis/DiagnosisGuide";
+import {
+  TEST_BOTTOM_SHEET_PAGE_PATH,
+  TestBottomSheetTutorialPage,
+} from "./pages/test/TestBottomSheetTutorialPage";
 
 function App() {
   return (
- <MobileResponsiveLayout>
+    <MobileResponsiveLayout>
       <BrowserRouter>
         <Routes>
           <Route exact element={<NavigationBarLayout />}>
             <Route exact path={"/"} element={<TestApp />} />
-            <Route exact path={TEST_BOTTOM_SHEET_PAGE_PATH} element={<TestBottomSheetTutorialPage />} />
+            <Route
+              exact
+              path={TEST_BOTTOM_SHEET_PAGE_PATH}
+              element={<TestBottomSheetTutorialPage />}
+            />
+            <Route
+              path={DIAGNOSIS_GUIDE_PAGE_PATH}
+              element={<DiagnosisGuide />}
+            />
           </Route>
           <Route path={SIGNUP_PAGE_PATH} element={<Signup />} />
           <Route path={SIGNIN_PAGE_PATH} element={<Signin />} />
         </Routes>
       </BrowserRouter>
     </MobileResponsiveLayout>
-  );  
+  );
 }
 
 export default App;
