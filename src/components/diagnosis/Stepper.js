54770b0f2ac5463f71c1a81a9fc52c70c85e9272
Hj1218 <wjdguswl2341@naver.com>
2024-07-16 21:46:08 +0900

    [publishing] 스테퍼 15개 데이터, 스크롤 추가

--- src/components/diagnosis/Stepper.js
+++ src/components/diagnosis/Stepper.js
@@ -1,10 +1,20 @@
 import React from "react";
 import {
-  TbCircleNumber1Filled,
-  TbCircleNumber2Filled,
-  TbCircleNumber3Filled,
-  TbCircleNumber4Filled,
-  TbCircleNumber5Filled,
+  TbNumber1Small,
+  TbNumber2Small,
+  TbNumber3Small,
+  TbNumber4Small,
+  TbNumber5Small,
+  TbNumber6Small,
+  TbNumber7Small,
+  TbNumber8Small,
+  TbNumber9Small,
+  TbNumber10Small,
+  TbNumber11Small,
+  TbNumber12Small,
+  TbNumber13Small,
+  TbNumber14Small,
+  TbNumber15Small,
   TbCircleCheck,
 } from "react-icons/tb";
 
@@ -12,15 +22,85 @@ const Stepper = ({ totalSteps, selectedButtons, onStepClick }) => {
   const getSize = () => (window.innerWidth >= 768 ? 50 : 30); // md 이상이면 50, 아니면 30
 
   const icons = [
-    <TbCircleNumber1Filled size={getSize()} color="#6100C1" />,
-    <TbCircleNumber2Filled size={getSize()} color="#6100C1" />,
-    <TbCircleNumber3Filled size={getSize()} color="#6100C1" />,
-    <TbCircleNumber4Filled size={getSize()} color="#6100C1" />,
-    <TbCircleNumber5Filled size={getSize()} color="#6100C1" />,
+    <TbNumber1Small
+      size={getSize()}
+      color="white"
+      className="border-2 rounded-full bg-[#6100C1]"
+    />,
+    <TbNumber2Small
+      size={getSize()}
+      color="white"
+      className="border-2 rounded-full bg-[#6100C1]"
+    />,
+    <TbNumber3Small
+      size={getSize()}
+      color="white"
+      className="border-2 rounded-full bg-[#6100C1] "
+    />,
+    <TbNumber4Small
+      size={getSize()}
+      color="white"
+      className="border-2 rounded-full bg-[#6100C1]"
+    />,
+    <TbNumber5Small
+      size={getSize()}
+      color="white"
+      className="border-2 rounded-full bg-[#6100C1] "
+    />,
+    <TbNumber6Small
+      size={getSize()}
+      color="white"
+      className="border-2 rounded-full bg-[#6100C1]"
+    />,
+    <TbNumber7Small
+      size={getSize()}
+      color="white"
+      className="border-2 rounded-full bg-[#6100C1]"
+    />,
+    <TbNumber8Small
+      size={getSize()}
+      color="white"
+      className="border-2 rounded-full bg-[#6100C1] "
+    />,
+    <TbNumber9Small
+      size={getSize()}
+      color="white"
+      className="border-2 rounded-full bg-[#6100C1]"
+    />,
+    <TbNumber10Small
+      size={getSize()}
+      color="white"
+      className="border-2 rounded-full bg-[#6100C1] "
+    />,
+    <TbNumber11Small
+      size={getSize()}
+      color="white"
+      className="border-2 rounded-full bg-[#6100C1]"
+    />,
+    <TbNumber12Small
+      size={getSize()}
+      color="white"
+      className="border-2 rounded-full bg-[#6100C1]"
+    />,
+    <TbNumber13Small
+      size={getSize()}
+      color="white"
+      className="border-2 rounded-full bg-[#6100C1] "
+    />,
+    <TbNumber14Small
+      size={getSize()}
+      color="white"
+      className="border-2 rounded-full bg-[#6100C1]"
+    />,
+    <TbNumber15Small
+      size={getSize()}
+      color="white"
+      className="border-2 rounded-full bg-[#6100C1] "
+    />,
   ];
 
   return (
-    <div className="w-full flex justify-center items-center my-16 cursor-pointer">
+    <div className="w-full flex items-center my-14 overflow-x-scroll scrollbar-hide px-5 md:px-10">
       {Array.from({ length: totalSteps }, (_, index) => (
         <div
           key={index}
@@ -30,7 +110,7 @@ const Stepper = ({ totalSteps, selectedButtons, onStepClick }) => {
           {selectedButtons[index] ? (
             <TbCircleCheck size={getSize()} color="#6100C1" />
           ) : (
-            icons[index]
+            <div className="relative">{icons[index]}</div>
           )}
           {index < totalSteps - 1 && <div className="w-12 md:w-20"></div>}
         </div>
--- src/pages/dementiaDiagnosis/Diagnosis.js
+++ src/pages/dementiaDiagnosis/Diagnosis.js
@@ -1,51 +1,56 @@
 import React, { useRef, useState } from "react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import "../../styles/sliderStyles.css";
-import Button from "../../components/Button";
-import DiagnosisSlider from "../../components/diagnosis/DiagnosisSlider";
-import Stepper from "../../components/diagnosis/Stepper";
+import "../../styles/sliderStyles.css"; // 슬라이더 스타일 파일
+import Button from "../../components/Button"; // 버튼 컴포넌트
+import DiagnosisSlider from "../../components/diagnosis/DiagnosisSlider"; // 진단 슬라이더 컴포넌트
+import Stepper from "../../components/diagnosis/Stepper"; // 스테퍼 컴포넌트
 
 export const DIAGNOSIS_PAGE_PATH = "/diagnosis";
 
 const Diagnosis = () => {
-  const sliderRef = useRef(null);
-  const [currentSlide, setCurrentSlide] = useState(0);
-  const [selectedButtons, setSelectedButtons] = useState(Array(5).fill(null));
-  const totalSlides = 5;
+  const sliderRef = useRef(null); // 슬라이더 ref
+  const [currentSlide, setCurrentSlide] = useState(0); // 현재 슬라이드 상태
+  const [selectedButtons, setSelectedButtons] = useState(Array(15).fill(null)); // 선택된 버튼 상태 배열 (15개)
+  const totalSlides = 15; // 총 슬라이드 개수
 
+  // 이전 버튼 클릭 핸들러
   const handlePrevClick = () => {
-    sliderRef.current.slickPrev();
+    sliderRef.current.slickPrev(); // React Slick의 이전 슬라이드 함수 호출
   };
 
+  // 다음(완료) 버튼 클릭 핸들러
   const handleNextClick = () => {
     if (currentSlide === totalSlides - 1) {
-      // 완료 버튼이 눌렸을 때, 체크되지 않은 슬라이더로 이동
+      // 마지막 슬라이드일 때
       const firstUncheckedSlide = selectedButtons.findIndex(
         (button) => button === null
-      );
+      ); // 체크되지 않은 첫 번째 슬라이드 찾기
       if (firstUncheckedSlide !== -1) {
-        sliderRef.current.slickGoTo(firstUncheckedSlide);
+        sliderRef.current.slickGoTo(firstUncheckedSlide); // 첫 번째 체크되지 않은 슬라이드로 이동
       } else {
         /* 진단이 완료된 경우 */
       }
     } else {
-      sliderRef.current.slickNext();
+      sliderRef.current.slickNext(); // 다음 슬라이드로 이동
     }
   };
 
+  // 슬라이드 변경 핸들러
   const handleSlideChange = (current) => {
-    setCurrentSlide(current);
+    setCurrentSlide(current); // 현재 슬라이드 상태 업데이트
   };
 
+  // 응답 처리 핸들러
   const handleResponse = (slideId, buttonId) => {
     const newSelectedButtons = [...selectedButtons];
-    newSelectedButtons[slideId] = buttonId;
+    newSelectedButtons[slideId] = buttonId; // 선택된 버튼 상태 업데이트
     setSelectedButtons(newSelectedButtons);
   };
 
+  // 스텝 클릭 핸들러
   const handleStepClick = (step) => {
-    sliderRef.current.slickGoTo(step);
+    sliderRef.current.slickGoTo(step); // 해당 슬라이드로 이동
   };
 
   return (
@@ -75,6 +80,7 @@ const Diagnosis = () => {
 
 export default Diagnosis;
 
+// 이전 버튼 컴포넌트
 const SliderPrevButton = ({ onClick }) => {
   return (
     <Button
@@ -85,6 +91,7 @@ const SliderPrevButton = ({ onClick }) => {
   );
 };
 
+// 다음(완료) 버튼 컴포넌트
 const SliderNextButton = ({ onClick, isLastSlide }) => {
   return (
     <Button
