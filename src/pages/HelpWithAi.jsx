795eb980ca1a942e013300ef982a623e8daaa4f9
이원찬 <twoone14@tukorea.ac.kr>
2024-07-18 08:49:35 +0900

    [feat] 채팅 관련 함수 useChatWithAi 커스텀 훅으로 분리
    [feat] 채팅 관련 함수 useChatWithAi 커스텀 훅으로 분리

--- src/pages/HelpWithAi.jsx
+++ src/pages/HelpWithAi.jsx
@@ -1,59 +1,53 @@
 import ChatSendIcon from "../components/helpWithAi/icon/ChatSendIcon";
-import { useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { useInput } from "../hooks/useInput";
-import ChatAiText from "../components/helpWithAi/ChatAiText";
-import ChatAiImage from "../components/helpWithAi/ChatAiImage";
-import ChatUserText from "../components/helpWithAi/ChatUserText";
+import useChatWithAi from "../hooks/HelpWithAi/useChatWithAi";
 
 export const HELP_WITH_AI_PATH = "/helpwithai";
 
 const HelpWithAi = () => {
-  const [chatLog, setChatLog] = useState([
-    {
-      Comp: ChatAiText,
-      text: "안녕하세요!",
-    },
-    {
-      Comp: ChatAiImage,
+  let {
+    ChatContainer,
+    appendChatUserText,
+    appendChatAiImage,
+    appendChatAiText,
+  } = useChatWithAi({ initChatLog: [] });
+
+  // dummy data
+  const [isFirstUseEffect, setIsFirstUseEffect] = useState(true);
+  useEffect(() => {
+    if (isFirstUseEffect) {
+      setIsFirstUseEffect(false);
+      return;
+    }
+    appendChatAiText({ text: "안녕하세요!" });
+    appendChatAiImage({
       urls: [
         "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
         "https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg",
       ],
-    },
-    {
-      Comp: ChatAiText,
-      text: "사진을 선택해 확대해 보세요!",
-    },
-  ]);
-
-  const chatContainerRef = useRef(null);
-
-  const appendUserChat = async ({ text }) => {
-    let newChat = { Comp: ChatUserText, text };
-    await setChatLog((pre) => [...pre, newChat]);
-    chatContainerRef.current.scrollTop = chatContainerRef.current.scrollHeight;
-  };
+    });
+    appendChatAiText({ text: "사진을 선택해 확대해 보세요!" });
+    appendChatUserText({ text: "오 좋은데요?" });
+  }, [isFirstUseEffect]);
 
   return (
     <>
-      <div className={"relative h-heightWithOutHeader flex flex-col px-4"}>
-        <div
-          ref={chatContainerRef}
-          className={
-            "p-4 flex flex-col gap-4 flex-1 bg-aiHelpButton rounded-t-lg-xl overflow-y-scroll"
-          }
-        >
-          {chatLog.map(({ Comp, ...props }, index) => (
-            <Comp key={index} {...props} />
-          ))}
-        </div>
-        <ChatBar appendUserChat={appendUserChat} />
+      <div
+        className={
+          "relative h-heightWithOutHeader flex flex-col px-12 mobile:px-4"
+        }
+      >
+        <ChatContainer
+          className={`p-4 flex flex-col gap-4 flex-1 bg-aiHelpButton rounded-t-lg-xl overflow-y-scroll`}
+        />
+        <ChatBar appendChatUserText={appendChatUserText} />
       </div>
     </>
   );
 };
 
-const ChatBar = ({ appendUserChat }) => {
+const ChatBar = ({ appendChatUserText }) => {
   let { form, handleChangeInput } = useInput({
     chatInput: "",
   });
@@ -61,7 +55,7 @@ const ChatBar = ({ appendUserChat }) => {
   let { chatInput } = form;
 
   const onSend = () => {
-    appendUserChat({ text: chatInput });
+    appendChatUserText({ text: chatInput });
   };
   return (
     <div className={"mb-4 flex h-14"}>
