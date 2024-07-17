7ad6c48c3dbeefd6b1fa654ac4c5bdc0a15781bf
이원찬 <twoone14@tukorea.ac.kr>
2024-07-18 08:23:26 +0900

    [publishing] 채팅창 전송 아이콘 구현

---
+++ src/components/helpWithAi/icon/ChatSendIcon.js
@@ -0,0 +1,20 @@
+const ChatSendIcon = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      fill="none"
+      viewBox="0 0 24 24"
+    >
+      <path
+        fill="#383838"
+        fillRule="evenodd"
+        d="M22 12a1 1 0 0 1-.638.932l-18 7a1 1 0 0 1-1.281-1.326L4.483 13H11a1 1 0 1 0 0-2H4.483L2.081 5.394a1 1 0 0 1 1.281-1.326l18 7A1 1 0 0 1 22 12Z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+};
+
+export default ChatSendIcon;
