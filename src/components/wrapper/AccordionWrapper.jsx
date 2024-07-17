fdd2d1ecc26e6bf01ac75be6139de57e2fe35134
이원찬 <twoone14@tukorea.ac.kr>
2024-07-17 20:22:23 +0900

    [feat] 아코디언 애니메이션 wrapper AccordionWrapper 구현

---
+++ src/components/wrapper/AccordionWrapper.jsx
@@ -0,0 +1,19 @@
+import { useRef } from "react";
+
+const AccordionWrapper = ({ children, isOpen, duration = 500 }) => {
+  const heightRef = useRef(null);
+  return (
+    <div
+      ref={heightRef}
+      style={{
+        height: isOpen ? `${heightRef.current?.scrollHeight}px` : "0px",
+        transitionDuration: `${duration}ms`,
+      }}
+      className={"mb-4 transition-all delay-0 overflow-clip"}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default AccordionWrapper;
