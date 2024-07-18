8b238dac69ed72d7f9313af7cd28e360c8a798cd
Hj1218 <wjdguswl2341@naver.com>
2024-07-18 23:21:03 +0900

    [publishing] stepper 움직이는 함수 분리

--- src/components/diagnosis/Stepper.js
+++ src/components/diagnosis/Stepper.js
@@ -42,28 +42,31 @@ const Stepper = ({ currentStep, totalSteps, selectedButtons, onStepClick }) => {
 
   useEffect(() => {
     if (scrollerRef.current) {
-      const selectedStepElement =
-        scrollerRef.current.children[
-          currentStep - 1
-        ]; /* 현재 슬라이드에 해당하는 step */
-      if (selectedStepElement) {
-        const containerWidth =
-          scrollerRef.current.offsetWidth; /* stepper 컨테이너 너비 */
-        const iconWidth = getSize(); /* 아이콘 너비 */
-        const iconOffsetLeft =
-          selectedStepElement.offsetLeft; /* 아이콘의 왼쪽 오프셋 위치 */
-        const scrollPosition =
-          iconOffsetLeft -
-          containerWidth / 2 +
-          iconWidth / 2; /* 스크롤 위치 계산하여 화면 중앙에 위치 */
-        scrollerRef.current.scrollTo({
-          left: scrollPosition,
-          behavior: "smooth",
-        });
-      }
+      moveStepper();
     }
   }, [currentStep]);
 
+  const moveStepper = () => {
+    const selectedStepElement =
+      scrollerRef.current.children[
+        currentStep - 1
+      ]; /* 현재 슬라이드에 해당하는 step */
+    if (selectedStepElement) {
+      const containerWidth =
+        scrollerRef.current.offsetWidth; /* stepper 컨테이너 너비 */
+      const iconWidth = getSize(); /* 아이콘 너비 */
+      const iconOffsetLeft =
+        selectedStepElement.offsetLeft; /* 아이콘의 왼쪽 오프셋 위치 */
+      const scrollPosition =
+        iconOffsetLeft -
+        containerWidth / 2 +
+        iconWidth / 2; /* 스크롤 위치 계산하여 화면 중앙에 위치 */
+      scrollerRef.current.scrollTo({
+        left: scrollPosition,
+        behavior: "smooth",
+      });
+    }
+  };
   return (
     <div
       className="w-full flex items-center my-14 overflow-x-scroll scrollbar-hide px-5 md:px-10"
