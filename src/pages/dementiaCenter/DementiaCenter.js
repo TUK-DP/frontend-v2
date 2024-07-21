e1f004676855845b85de1ae68e7f4ab0545a3345
Hj1218 <wjdguswl2341@naver.com>
2024-07-21 22:20:00 +0900

    [publishing] 전체동작수정

--- src/pages/dementiaCenter/DementiaCenter.js
+++ src/pages/dementiaCenter/DementiaCenter.js
@@ -5,39 +5,39 @@ import Search from "../../assets/dementiaCenter/search.png";
 export const DEMENTIA_CENTER_PAGE_PATH = "/dementia/center";
 
 const DementiaCenter = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [centers, setCenters] = useState([]);
+  const [isSearchClicked, setIsSearchClicked] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
+
+  const handleSearch = () => {
+    setCenters([]);
+    setIsSearchClicked(true);
+  };
+
   return (
-    <div className={"flex flex-col h-[99%] px-2.5 pt-5"}>
-      <SearchBar />
-      <CenterList />
+    <div className={"flex flex-col h-[99%] pt-5"}>
+      <SearchBar isLoading={isLoading} onSearch={handleSearch} />
+      {!isLoading && (
+        <CenterList centers={centers} isSearchClicked={isSearchClicked} />
+      )}
     </div>
   );
 };
 
 export default DementiaCenter;
 
-const SearchBar = () => {
-  const [isLoading, setIsLoading] = useState(false);
-
-  useEffect(() => {
-    setIsLoading(true);
-    const timer = setTimeout(() => {
-      setIsLoading(false);
-    }, 3000);
-  }, []);
-
+const SearchBar = ({ isLoading, onSearch }) => {
   return (
-    <div className="flex items-center w-auto h-12 md:h-16 px-4 md:px-5 border-[1px] rounded-3xl border-black">
-      <InputComp isLoading={isLoading} />
-      {isLoading && <Spinner />}
-    </div>
-  );
-};
-
-const InputComp = ({ isLoading }) => {
-  return (
-    <>
+    <div className="mx-2.5 mb-3 flex items-center w-auto h-12 md:h-16 px-4 md:px-5 border-[1px] rounded-3xl border-black">
       <input
-        className="border-none flex-1 text-lg md:text-2xl"
+        className="border-none flex-1 text-lg md:text-2xl outline-none"
         type={"number"}
         placeholder={
           isLoading
@@ -45,16 +45,15 @@ const InputComp = ({ isLoading }) => {
             : "거리(km)를 입력해주세요."
         }
       />
-      <SearchIcon isLoading={isLoading} />
-    </>
-  );
-};
-
-const SearchIcon = ({ isLoading }) => {
-  return (
-    !isLoading && (
-      <img src={Search} className={"h-9 w-9 cursor-pointer"} alt={""} />
-    )
+      {!isLoading && (
+        <img
+          src={Search}
+          className={"h-9 w-9 cursor-pointer"}
+          onClick={onSearch}
+        />
+      )}
+      {isLoading && <Spinner />}
+    </div>
   );
 };
 
