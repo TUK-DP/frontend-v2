afaeaa1edfc0c9b9388b4c17fd241269f4702ed3
이원찬 <twoone14@tukorea.ac.kr>
2024-07-24 13:54:50 +0900

    [feat] react query, skeleton 을 이용한 달력 일기 check api 구현

--- src/components/calendar/CalendarGrid.jsx
+++ src/components/calendar/CalendarGrid.jsx
@@ -4,6 +4,14 @@ import {
   isSaturday,
   isSunday,
 } from "../../utils/calendar/date";
+import { useQuery } from "@tanstack/react-query";
+import DiaryController from "../../apis/diary.controller";
+import {
+  dateToDotString,
+  HOUR,
+  yearMonthToDashString,
+} from "../../utils/api/dateConverter";
+import { ResponseSkeleton } from "../skeleton/ResponseSkeleton";
 
 export const CalendarGrid = ({
   days,
@@ -12,29 +20,23 @@ export const CalendarGrid = ({
   setSelectedDate,
 }) => {
   return (
-    <div className={"grid grid-cols-7 text-4xl mobile:text-2xl"}>
-      <CalendarHeader />
-      <CalenderBody
-        days={days}
-        selectedYearMonth={selectedYearMonth}
-        selectedDate={selectedDate}
-        setSelectedDate={setSelectedDate}
-      />
-    </div>
+    <>
+      <div className={"grid grid-cols-7 gap-2 text-4xl mobile:text-2xl"}>
+        <CalendarHeader />
+        <CalenderBody
+          days={days}
+          selectedYearMonth={selectedYearMonth}
+          selectedDate={selectedDate}
+          setSelectedDate={setSelectedDate}
+        />
+      </div>
+    </>
   );
 };
 
 const CalendarHeader = () => {
   return CALENDAR_HEADER.map((day, index) => (
-    <CalendarCell key={day}>
-      <span
-        className={`font-black mb-2 
-          ${isSaturday(index) && "text-blue-600"} 
-          ${isSunday(index) && "text-red-600"}`}
-      >
-        {day}
-      </span>
-    </CalendarCell>
+    <CalendarHeaderCell key={day} day={day} index={index} />
   ));
 };
 
@@ -53,6 +55,59 @@ const CalenderBody = ({
     });
   };
 
+  return days.map((day, index) => (
+    <CalendarBodyCell
+      key={dateToDotString({ ...selectedYearMonth, day: index })}
+      day={day}
+      selectedYearMonth={selectedYearMonth}
+      selectedDate={selectedDate}
+      handleClickCell={handleClickCell}
+    />
+  ));
+};
+
+const CalendarHeaderCell = ({ index, day, className }) => {
+  return (
+    <div
+      className={`flex flex-col justify-center items-center w-full h-full ${className}`}
+    >
+      <span
+        className={`font-black mb-2 
+          ${isSaturday(index) && "text-blue-600"} 
+          ${isSunday(index) && "text-red-600"}`}
+      >
+        {day}
+      </span>
+    </div>
+  );
+};
+
+const CalendarBodyCell = ({
+  selectedYearMonth,
+  selectedDate,
+  className = "",
+  handleClickCell,
+  day,
+  index,
+}) => {
+  let {
+    isFetching,
+    isSuccess,
+    data: diaryChecks,
+  } = useQuery({
+    queryKey: [
+      "diaries",
+      { year: selectedYearMonth.year, month: selectedYearMonth.month },
+    ],
+    queryFn: () =>
+      DiaryController.findCheckDiaries({
+        userId: 2,
+        year: selectedYearMonth.year,
+        month: selectedYearMonth.month,
+      }),
+    staleTime: HOUR * 2,
+  });
+
   const isSelectedCell = (day) => {
     return isEqualDate(selectedDate, {
       ...selectedYearMonth,
@@ -60,33 +115,42 @@ const CalenderBody = ({
     });
   };
 
-  const getKey = (selectedMonth, index) => {
-    return `${selectedMonth.year}.${selectedMonth.month}.${index}`;
-  };
-
-  return days.map((day, index) => (
-    <CalendarCell key={getKey(selectedYearMonth, index)}>
-      <div
-        className={`flex justify-center rounded-full items-center cursor-pointer px-1 w-16 h-16
+  return (
+    <div
+      className={`relative flex flex-col justify-center items-center w-full ${className}`}
+    >
+      {isFetching && <ResponseSkeleton />}
+      {!isFetching && isSuccess && (
+        <>
+          <div
+            className={`flex flex-col justify-center items-center rounded-full cursor-pointer px-1 w-16 h-16
           mobile:w-10 mobile:h-10
           ${isSunday(index) && "text-red-600"}
           ${isSaturday(index) && "text-blue-600"}
           ${isSelectedCell(day) && "bg-blue-600 !text-white"}
         `}
-        onClick={() => handleClickCell(day)}
-      >
-        {day}
-      </div>
-    </CalendarCell>
-  ));
+            onClick={() => handleClickCell(day)}
+          >
+            {day}
+          </div>
+          <Dot
+            isVisible={
+              diaryChecks.data.result[
+                yearMonthToDashString({ ...selectedYearMonth })
+              ][day]?.isExist
+            }
+            isSelected={isSelectedCell(day)}
+          />
+        </>
+      )}
+    </div>
+  );
 };
 
-const CalendarCell = ({ className = "", children }) => {
+const Dot = ({ isVisible, isSelected }) => {
   return (
-    <div
-      className={`flex justify-center items-center w-full h-full ${className}`}
-    >
-      {children}
-    </div>
+    <span
+      className={`absolute bottom-0 w-2 h-2 bg-primary-600 rounded-full invisible ${isVisible && "!visible"} ${isSelected && "!bg-white"}`}
+    />
   );
 };
