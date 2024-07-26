a0992320077ce831166df699755323089b95a1bb
Hj1218 <wjdguswl2341@naver.com>
2024-07-26 14:39:28 +0900

    [publishing] 페이지 생성 및 라우트 연결

---
+++ src/pages/dementiaCenter/CenterMap.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const CENTER_MAP_PAGE_PATH = "/dementia/center/map";
+
+const CenterMap = () => {
+  return <div>CenterMap</div>;
+};
+
+export default CenterMap;
