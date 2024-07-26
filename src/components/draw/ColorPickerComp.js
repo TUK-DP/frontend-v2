78831ab67d8405fc413e0c82dfe4fd5d44d401ac
정현지 <114800998+Hj1218@users.noreply.github.com>
2024-07-26 22:31:10 +0900

    Publishing/23 Color Picker 모달 퍼블리싱 (#97)
    * [publishing] 색상 선택 문구 추가, 버튼 색상 변경
    
    * [publishing] 반응형 추가
    
    * [publishing] 매직넘버 사용 x

--- src/components/draw/ColorPickerComp.js
+++ src/components/draw/ColorPickerComp.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ColorPicker } from "react-color-palette";
 import "react-color-palette/css";
+import { pickerHeight, mdPickerHeight } from "../../constants/size";
 
 const ColorPickerComp = ({
   color,
@@ -8,18 +9,37 @@ const ColorPickerComp = ({
   setSelectedColor,
   setColorPickerOpen,
 }) => {
+  const [colorPickerHeight, setColorPickerHeight] = useState(pickerHeight);
+
+  useEffect(() => {
+    const updateHeight = () => {
+      if (window.innerWidth >= 768) {
+        setColorPickerHeight(mdPickerHeight);
+      } else {
+        setColorPickerHeight(pickerHeight);
+      }
+    };
+
+    updateHeight();
+  }, []);
+  console.log(colorPickerHeight);
+
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg w-[20rem] md:w-[35rem]">
+      <div className="bg-white p-6 rounded-lg shadow-lg w-[20rem] md:w-[35rem] md:h-[30rem]">
+        <div className="w-full font-bold text-xl md:text-3xl flex justify-center mb-3">
+          원하는 색상을 선택하세요
+        </div>
         <ColorPicker
           color={color}
           onChange={setColor}
           hideInput={["rgb", "hsv", "hex"]} // 옵션 숨기기
+          height={colorPickerHeight}
         />
         <div className="flex items-center justify-between mt-6">
           <ColorState color={color} />
           <button
-            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded-md shadow-md transition duration-300"
+            className="bg-[#6100C1] hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded-md shadow-md transition duration-300 md:text-2xl"
             onClick={() => {
               setSelectedColor(color.hex);
               setColorPickerOpen(false);
@@ -38,9 +58,9 @@ export default ColorPickerComp;
 const ColorState = ({ color }) => {
   return (
     <div className="flex items-center">
-      <strong className="md:text-xl">선택한 색상:</strong>
+      <strong className="md:text-2xl">선택한 색상:</strong>
       <div
-        className="w-20 md:w-28 h-8 border rounded-md ml-2 md:ml-5 shadow-inner"
+        className="w-20 md:w-28 h-8 md:h-10 border rounded-md ml-2 md:ml-8"
         style={{ backgroundColor: color.hex }}
       ></div>
     </div>
