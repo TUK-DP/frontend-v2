e1a96358ff9aa5d9767614963413d9c98bceb048
Hj1218 <wjdguswl2341@naver.com>
2024-07-26 16:08:35 +0900

    [publishing] 반응형 추가

--- src/components/draw/ColorPickerComp.js
+++ src/components/draw/ColorPickerComp.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { ColorPicker } from "react-color-palette";
 import "react-color-palette/css";
 
@@ -8,9 +8,22 @@ const ColorPickerComp = ({
   setSelectedColor,
   setColorPickerOpen,
 }) => {
+  const [pickerHeight, setPickerHeight] = useState(200);
+  useEffect(() => {
+    const updateHeight = () => {
+      if (window.innerWidth >= 768) {
+        setPickerHeight(240);
+      } else {
+        setPickerHeight(200);
+      }
+    };
+
+    // Set the initial height
+    updateHeight();
+  }, []);
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg w-[20rem] md:w-[35rem]">
+      <div className="bg-white p-6 rounded-lg shadow-lg w-[20rem] md:w-[35rem] md:h-[30rem]">
         <div className="w-full font-bold text-xl md:text-3xl flex justify-center mb-3">
           원하는 색상을 선택하세요
         </div>
@@ -18,11 +31,12 @@ const ColorPickerComp = ({
           color={color}
           onChange={setColor}
           hideInput={["rgb", "hsv", "hex"]} // 옵션 숨기기
+          height={pickerHeight}
         />
         <div className="flex items-center justify-between mt-6">
           <ColorState color={color} />
           <button
-            className="bg-[#6100C1] hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded-md shadow-md transition duration-300"
+            className="bg-[#6100C1] hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded-md shadow-md transition duration-300 md:text-2xl"
             onClick={() => {
               setSelectedColor(color.hex);
               setColorPickerOpen(false);
@@ -43,7 +57,7 @@ const ColorState = ({ color }) => {
     <div className="flex items-center">
       <strong className="md:text-2xl">선택한 색상:</strong>
       <div
-        className="w-20 md:w-28 h-8 border rounded-md ml-2 md:ml-5 shadow-inner"
+        className="w-20 md:w-28 h-8 md:h-10 border rounded-md ml-2 md:ml-8"
         style={{ backgroundColor: color.hex }}
       ></div>
     </div>
