aa2582af82c1c881059be1bdc5cf4ef69ead478d
Hj1218 <wjdguswl2341@naver.com>
2024-07-26 14:55:36 +0900

    [publishing] 페이지 퍼블리싱

--- src/pages/dementiaCenter/CenterMap.jsx
+++ src/pages/dementiaCenter/CenterMap.jsx
@@ -1,9 +1,32 @@
 import React from "react";
+import { FaRegBuilding } from "react-icons/fa";
+import { MdOutlineWatchLater } from "react-icons/md";
 
 export const CENTER_MAP_PAGE_PATH = "/dementia/center/map";
 
 const CenterMap = () => {
-  return <div>CenterMap</div>;
+  return (
+    <div className="mx-6">
+      <div className="text-center text-2xl font-bold mb-6">center.name</div>
+      <div className="bg-[#d9d9d9] h-[30rem]">kakaoMap</div>
+      <CenterInfo address={"center.address"} time={"center.time"} />
+    </div>
+  );
 };
 
 export default CenterMap;
+
+const CenterInfo = ({ address, time }) => {
+  return (
+    <div className="text-xl mt-5">
+      <div className="flex items-center mb-3">
+        <FaRegBuilding className="mr-3" />
+        {address}
+      </div>
+      <div className="flex items-center">
+        <MdOutlineWatchLater className="mr-3" />
+        {time}
+      </div>
+    </div>
+  );
+};
