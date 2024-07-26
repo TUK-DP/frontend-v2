00340f3d552ceed266af396c7c7c7d2d7e95411a
Hj1218 <wjdguswl2341@naver.com>
2024-07-26 15:03:59 +0900

    [publishing] 반응형 추가

--- src/pages/dementiaCenter/CenterMap.jsx
+++ src/pages/dementiaCenter/CenterMap.jsx
@@ -6,9 +6,11 @@ export const CENTER_MAP_PAGE_PATH = "/dementia/center/map";
 
 const CenterMap = () => {
   return (
-    <div className="mx-6">
-      <div className="text-center text-2xl font-bold mb-6">center.name</div>
-      <div className="bg-[#d9d9d9] h-[30rem]">kakaoMap</div>
+    <div className="mx-6 tablet:mx-12">
+      <div className="text-center text-2xl tablet:text-4xl font-bold mb-6 tablet:mb-10">
+        center.name
+      </div>
+      <div className="bg-[#d9d9d9] h-[30rem] tablet:h-[45rem]">kakaoMap</div>
       <CenterInfo address={"center.address"} time={"center.time"} />
     </div>
   );
@@ -18,8 +20,8 @@ export default CenterMap;
 
 const CenterInfo = ({ address, time }) => {
   return (
-    <div className="text-xl mt-5">
-      <div className="flex items-center mb-3">
+    <div className="text-xl tablet:text-3xl mt-5 tablet:mt-8">
+      <div className="flex items-center mb-3 tablet:mb-6">
         <FaRegBuilding className="mr-3" />
         {address}
       </div>
