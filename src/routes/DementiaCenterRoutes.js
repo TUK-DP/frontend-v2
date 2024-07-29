f8e5b488c4906dbf20125d3ef778d6c17c5bdbb0
Hj1218 <wjdguswl2341@naver.com>
2024-07-30 00:21:38 +0900

    [feat] route 추가

--- src/routes/DementiaCenterRoutes.js
+++ src/routes/DementiaCenterRoutes.js
@@ -3,11 +3,15 @@ import { Route } from "react-router-dom";
 import CenterMap, {
   CENTER_MAP_PAGE_PATH,
 } from "../pages/dementiaCenter/CenterMap";
+import DementiaCenter, {
+  DEMENTIA_CENTER_PAGE_PATH,
+} from "../pages/dementiaCenter/DementiaCenter";
 
 const DementiaCenterRoutes = () => {
   return (
     <>
       <Route path={CENTER_MAP_PAGE_PATH} element={<CenterMap />} />
+      <Route path={DEMENTIA_CENTER_PAGE_PATH} element={<DementiaCenter />} />
     </>
   );
 };
