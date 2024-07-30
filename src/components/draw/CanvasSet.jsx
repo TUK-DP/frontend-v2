94428688fb1b69affc0a674ba15718883602e634
heedong12 <dlgmldus3289@naver.com>
2024-07-30 17:46:02 +0900

    [feat]캔버스 상태 전역으로 저장

--- src/components/draw/CanvasSet.jsx
+++ src/components/draw/CanvasSet.jsx
@@ -1,10 +1,13 @@
-import React, { useRef, useState, useEffect } from "react";
+import React, { useEffect, useRef } from "react";
+import { useDrawEvents } from "../../hooks/canvas/useDrawEvents";
 import { useDrawingToolStore } from "../../stores/DrawingToolStore";
 
-const CanvasSet = ({ canvasRef, canvasBgRef, canvasWidth }) => {
+const CanvasSet = ({ canvasWidth }) => {
+  const canvasRef = useRef(null);
+
   return (
     <div className="w-full aspect-square bg-white">
-      <DrawCanvas canvasWidth={canvasWidth} />
+      <DrawCanvas canvasWidth={canvasWidth} canvasRef={canvasRef} />
       {/* <BackgroundCanvas ref={canvasBgRef}/> */}
     </div>
   );
@@ -12,135 +15,17 @@ const CanvasSet = ({ canvasRef, canvasBgRef, canvasWidth }) => {
 
 export default CanvasSet;
 
-const DrawCanvas = ({ canvasWidth }) => {
-  const { drawingTools } = useDrawingToolStore();
-  const { brushSize, color, drawingMode } = drawingTools;
-  const canvasRef = useRef(null);
-  const [context, setContext] = useState(null);
-  const [isDrawing, setIsDrawing] = useState(false);
-  const [history, setHistory] = useState([]);
-  const [redoList, setRedoList] = useState([]);
+const DrawCanvas = ({ canvasWidth, canvasRef }) => {
+  const { startDrawing, endDrawing, draw } = useDrawEvents(canvasRef);
 
-  // 초기 설정
   useEffect(() => {
-    const canvas = canvasRef.current;
-    const ctx = canvas.getContext("2d");
-    setContext(ctx);
-    ctx.lineWidth = brushSize;
-    ctx.lineCap = "round";
-    ctx.strokeStyle = color;
-  }, []);
-
-  // 색과 두께 변경시 마다 호출
-  useEffect(() => {
-    let canvas = canvasRef.current;
-    let ctx = canvas.getContext("2d");
-    ctx.lineWidth = brushSize;
-    ctx.strokeStyle = color;
-  }, [brushSize, color]);
-
-  // 그리기 시작
-  const startDrawing = (event) => {
-    setIsDrawing(true);
-    draw(event);
-  };
-
-  // 그리기 종료
-  const endDrawing = () => {
-    setIsDrawing(false);
-    context?.beginPath();
-    updateCanvasState();
-    //redoList 초기화
-    if (redoList.length > 0) {
-      setRedoList([]);
+    console.log("CanvasRef:", canvasRef.current);
+    if (canvasRef.current) {
+      const ctx = canvasRef.current.getContext("2d");
+      console.log("Canvas Context:", ctx);
     }
-  };
+  }, [canvasRef]);
 
-  // 실제 그리기 함수
-  const draw = (event) => {
-    if (!context) return;
-
-    const canvas = canvasRef.current;
-    const rect = canvas.getBoundingClientRect();
-
-    // 터치 이벤트
-    let x = event.clientX - rect.left;
-    let y = event.clientY - rect.top;
-
-    // 터치 이벤트
-    if (event.touches) {
-      const touch = event.touches[0];
-      const rect = canvas.getBoundingClientRect();
-      x = touch.clientX - rect.left;
-      y = touch.clientY - rect.top;
-    }
-
-    // 지우기
-    if (!drawingMode) {
-      context.clearRect(
-        x - brushSize / 2,
-        y - brushSize / 2,
-        brushSize,
-        brushSize
-      );
-    }
-
-    // 그리기
-    if (drawingMode && isDrawing) {
-      context.lineWidth = brushSize;
-      context.lineCap = "round";
-      context.strokeStyle = color;
-
-      context.lineTo(x, y);
-      context.stroke();
-      context.beginPath();
-      context.moveTo(x, y);
-    }
-  };
-
-  // 캔버스 상태를 히스토리에 업데이트하는 함수
-  const updateCanvasState = () => {
-    let canvas = canvasRef.current;
-    let ctx = canvas.getContext("2d");
-    const currentState = ctx.getImageData(0, 0, canvas.width, canvas.height);
-    setHistory((prevHistory) => [...prevHistory, currentState]);
-  };
-
-  // undo 기능
-  const undo = () => {
-    if (history.length > 0) {
-      const previousState = history[history.length - 1];
-      setRedoList((prevRedoHistory) => [
-        context.getImageData(
-          0,
-          0,
-          canvasRef.current.width,
-          canvasRef.current.height
-        ),
-        ...prevRedoHistory,
-      ]);
-      setHistory((prevHistory) => prevHistory.slice(0, -1));
-      context.putImageData(previousState, 0, 0);
-    }
-  };
-
-  // redo 기능
-  const redo = () => {
-    if (redoList.length > 0) {
-      const nextState = redoList[0];
-      setHistory((prevHistory) => [
-        context.getImageData(
-          0,
-          0,
-          canvasRef.current.width,
-          canvasRef.current.height
-        ),
-        ...prevHistory,
-      ]);
-      setRedoList((prevRedoHistory) => prevRedoHistory.slice(1));
-      context.putImageData(nextState, 0, 0);
-    }
-  };
   return (
     <canvas
       ref={canvasRef}
