4acbff3707198d7102a27a2064b47d6548e3f893
정현지 <114800998+Hj1218@users.noreply.github.com>
2024-07-30 23:40:24 +0900

    Feat/74 주변치매센터 연동 (#103)
    * [feat] center controller 작성
    
    * [feat] useGetNearByCenter hook 작성
    
    * [feat] distance 소수점 첫째 자리까지로 변경
    
    * [feat] route 추가
    
    * [feat] 주변치매센터 api 연동

--- src/components/dementiaCenter/CenterList.js
+++ src/components/dementiaCenter/CenterList.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { CENTER_MAP_PAGE_PATH } from "../../pages/dementiaCenter/CenterMap";
 
-const CenterList = ({ centers, isSearchClicked }) => {
+const CenterList = ({
+  centers,
+  isSearchClicked,
+  isPositionFetchingDone,
+  isCenterDataFetchingDone,
+}) => {
   return (
     <>
-      <NoneCentersComp centers={centers} isSearchClicked={isSearchClicked} />
+      <NoneCentersComp
+        centers={centers}
+        isSearchClicked={isSearchClicked}
+        isPositionFetchingDone={isPositionFetchingDone}
+        isCenterDataFetchingDone={isCenterDataFetchingDone}
+      />
       <div className="overflow-scroll scrollbar-hide">
         {centers.length > 0 && <CenterComp centers={centers} />}
       </div>
@@ -12,7 +24,15 @@ const CenterList = ({ centers, isSearchClicked }) => {
 };
 export default CenterList;
 
-const NoneCentersComp = ({ centers, isSearchClicked }) => {
+const NoneCentersComp = ({
+  centers,
+  isSearchClicked,
+  isPositionFetchingDone,
+  isCenterDataFetchingDone,
+}) => {
+  if (!isPositionFetchingDone) {
+    return null;
+  }
   if (!isSearchClicked) {
     return (
       <div className={"text-center mt-4 md:text-2xl"}>
@@ -20,7 +40,7 @@ const NoneCentersComp = ({ centers, isSearchClicked }) => {
       </div>
     );
   }
-  if (isSearchClicked && centers.length === 0) {
+  if (isSearchClicked && isCenterDataFetchingDone && centers.length === 0) {
     return (
       <div className={"text-center mt-4 md:text-2xl"}>검색 결과가 없습니다</div>
     );
@@ -29,15 +49,21 @@ const NoneCentersComp = ({ centers, isSearchClicked }) => {
 };
 
 const CenterComp = ({ centers }) => {
+  const navigate = useNavigate();
+
+  const onClick = (center) => {
+    navigate(CENTER_MAP_PAGE_PATH, { state: { center } });
+  };
   return (
     <div className="flex-col text-lg ">
       {centers.map((center, index) => (
         <div
+          onClick={() => onClick(center)}
           key={index}
           className={`flex flex-col h-24 md:h-32 px-3 md:px-6 justify-evenly ${index % 2 === 1 ? "bg-[#F7F7F7]" : ""}`}
         >
           <div className="flex justify-between font-bold text-xl md:text-3xl">
-            <span>{center.centerName}</span>
+            <span>{center.name}</span>
             <span>{center.distance} km</span>
           </div>
           <div className={"text-[#686868] md:text-2xl"}>{center.address}</div>
--- src/pages/dementiaCenter/CenterMap.jsx
+++ src/pages/dementiaCenter/CenterMap.jsx
@@ -1,27 +1,30 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 import { FaRegBuilding } from "react-icons/fa";
-import { MdOutlineWatchLater } from "react-icons/md";
+import { IoCall } from "react-icons/io5";
 import KakaoMap from "../../components/dementiaCenter/KakaoMap";
 
 export const CENTER_MAP_PAGE_PATH = "/dementia/center/map";
 
 const CenterMap = () => {
+  const location = useLocation();
+  const { center } = location.state;
   return (
     <div className="mx-6 tablet:mx-12">
       <div className="text-center text-2xl tablet:text-4xl font-bold mb-6 tablet:mb-10">
-        center.name
+        {center.name}
       </div>
       <div className="h-[30rem] tablet:h-[45rem]">
-        <KakaoMap />
+        <KakaoMap latitude={center.latitude} longitude={center.longitude} />
       </div>
-      <CenterInfo address={"center.address"} time={"center.time"} />
+      <CenterInfo address={center.address} callnumber={center.callnumber} />
     </div>
   );
 };
 
 export default CenterMap;
 
-const CenterInfo = ({ address, time }) => {
+const CenterInfo = ({ address, callnumber }) => {
   return (
     <div className="text-xl tablet:text-3xl mt-5 tablet:mt-8">
       <div className="flex items-center mb-3 tablet:mb-6">
@@ -29,8 +32,8 @@ const CenterInfo = ({ address, time }) => {
         {address}
       </div>
       <div className="flex items-center">
-        <MdOutlineWatchLater className="mr-3" />
-        {time}
+        <IoCall className="mr-3" />
+        {callnumber}
       </div>
     </div>
   );
