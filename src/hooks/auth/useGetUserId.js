408cc6c8956215bc78b2329d30469e8481b7dc74
이원찬 <twoone14@tukorea.ac.kr>
2024-08-01 23:41:04 +0900

    [feat] useGetUserId 작성

---
+++ src/hooks/auth/useGetUserId.js
@@ -0,0 +1,18 @@
+import useUserStore from "../../stores/UserStore";
+import { useNavigate } from "react-router-dom";
+import { SIGNIN_PAGE_PATH } from "../../pages/auths/Signin";
+import { useEffect } from "react";
+
+const useGetUserId = () => {
+  const navigate = useNavigate();
+  const { isLogin, userId } = useUserStore((state) => state);
+  useEffect(() => {
+    if (!isLogin || userId === 0) {
+      navigate(SIGNIN_PAGE_PATH);
+    }
+  }, [isLogin, userId]);
+
+  return { userId: isLogin ? userId : 0, isLogin };
+};
+
+export default useGetUserId;
