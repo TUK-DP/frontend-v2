f675e4d748acf107666781281dab05805701386b
heedong12 <dlgmldus3289@naver.com>
2024-08-02 17:45:52 +0900

    [feat] 키워드 없을 때, 기본 값 설정

--- src/hooks/canvas/useFetchKeyword.js
+++ src/hooks/canvas/useFetchKeyword.js
@@ -4,6 +4,12 @@ const useFetchKeywords = () => {
   const { diary } = useFetchDiary();
   let keywords = diary?.keywords ?? [];
 
+  if (diary.keywords.length === 0) {
+    keywords = [
+      { keyword: "자유롭게 그려주세요!", keywordId: 0, imgUrl: null },
+    ];
+  }
+
   return { keywords };
 };
 
