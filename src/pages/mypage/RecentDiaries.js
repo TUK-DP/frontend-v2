7fe05a4f219c4afc3322471762a49743e51d20b3
이원찬 <twoone14@tukorea.ac.kr>
2024-08-03 20:09:33 +0900

    [feat] 최근 일기 무한 스크롤 구현

--- src/pages/mypage/RecentDiaries.js
+++ src/pages/mypage/RecentDiaries.js
@@ -1,73 +1,71 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useRequireAuth from "../../hooks/auth/useRequireAuth";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { generateRecentMock } from "../../utils/mock/diary/diaryMockData";
+import { MINUTE } from "../../utils/api/dateConverter";
+import useScreenOn from "../../hooks/common/useScreenOn";
+import { range } from "../../utils/array/range";
+import { ResponseSkeleton } from "../../components/skeleton/ResponseSkeleton";
 
 export const RECENT_DIARIES_PAGE_PATH = "/recentdiaries";
 
 const RecentDiaries = () => {
-  const MonthlyDiaryData = [
-    {
-      year: 2024,
-      months: [
-        {
-          month: 7,
-          records: [
-            { date: "2024-07-17" },
-            { date: "2024-07-09" },
-            { date: "2024-07-02" },
-          ],
-        },
-        {
-          month: 6,
-          records: [
-            { date: "2024-06-24" },
-            { date: "2024-06-18" },
-            { date: "2024-06-15" },
-          ],
-        },
-      ],
-    },
-    {
-      year: 2023,
-      months: [
-        {
-          month: 12,
-          records: [
-            { date: "2023-12-16" },
-            { date: "2023-12-08" },
-            { date: "2023-12-04" },
-          ],
-        },
-      ],
-    },
-  ];
+  const { userId, isLogin } = useRequireAuth();
+
+  let { data, isFetching, fetchNextPage, isSuccess, hasNextPage } =
+    useInfiniteQuery({
+      queryKey: ["diary", "recent"],
+      queryFn: async ({ pageParam }) => {
+        const response = await generateRecentMock(pageParam, 6);
+        return response.data.result;
+      },
+      initialPageParam: 1,
+      getNextPageParam: (lastPage, allPages, lastPageParam) => {
+        if (lastPage.hasNext) return lastPageParam + 1;
+        return undefined;
+      },
+      enabled: isLogin,
+      staleTime: MINUTE,
+      gcTime: 5 * MINUTE,
+    });
+
+  const { isOnScreen, targetRef } = useScreenOn({
+    marginOffset: 30,
+  });
+
+  useEffect(() => {
+    if (!isOnScreen || isFetching || !hasNextPage) return;
+    fetchNextPage();
+  }, [isOnScreen, isFetching, isSuccess]);
 
   return (
-    <div>
-      <MonthlyDiaryList data={MonthlyDiaryData} />
+    <div className="ml-10 md:ml-20 pb-6">
+      {isSuccess &&
+        Object.entries(convertData(data)).map(([yearMonth, diaryArr]) => {
+          const [year, month] = yearMonth.split("-");
+          return (
+            <div key={yearMonth} className="my-8 md:my-16">
+              <MonthlyTitle year={year} month={month} />
+              {diaryArr.map((diary) => (
+                <DiaryItem key={diary.diaryId} diary={diary} />
+              ))}
+            </div>
+          );
+        })}
+      <div className={"flex flex-col gap-8"}>
+        {isFetching &&
+          range(6).map(() => (
+            <div className={"h-12 overflow-clip rounded-lg-xl"}>
+              <ResponseSkeleton />
+            </div>
+          ))}
+      </div>
+      <div ref={targetRef} />
     </div>
   );
 };
 
 export default RecentDiaries;
-
-const MonthlyDiaryList = ({ data }) => {
-  return (
-    <div className="ml-10 md:ml-20 pb-6">
-      {data.map((yearData) =>
-        yearData.months.map((monthData) => (
-          <div
-            key={`${yearData.year}-${monthData.month}`}
-            className="my-8 md:my-16"
-          >
-            <MonthlyTitle year={yearData.year} month={monthData.month} />
-            {monthData.records.map((record, index) => (
-              <DiaryItem key={index} date={record.date} />
-            ))}
-          </div>
-        ))
-      )}
-    </div>
-  );
-};
 
 const MonthlyTitle = ({ year, month }) => {
   return (
@@ -80,18 +78,23 @@ const MonthlyTitle = ({ year, month }) => {
   );
 };
 
-const DiaryItem = ({ date }) => {
+const DiaryItem = ({ diary }) => {
+  const { createDate } = diary;
+  const [year, month, day] = createDate.split("-");
   return (
     <div className="text-xl md:text-3xl my-9 flex justify-between pr-5">
-      {formatDate(date)}
+      {`${Number(month)}월 ${Number(day)}일의 일기`}
       <button>확인하기 {">"} </button>
     </div>
   );
 };
 
-const formatDate = (dateString) => {
-  const date = new Date(dateString);
-  const month = date.getMonth() + 1;
-  const day = date.getDate();
-  return `${month}월 ${day}일의 일기`;
+const convertData = (data) => {
+  return Object.groupBy(
+    data.pages
+      .flat()
+      .map((response) => response.diaryList)
+      .flat(),
+    ({ createDate }) => createDate.slice(0, 7)
+  );
 };
