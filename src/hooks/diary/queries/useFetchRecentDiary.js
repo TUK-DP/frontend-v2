a46989767b340ef7cee51fdf3cdd9305e0341b23
이원찬 <twoone14@tukorea.ac.kr>
2024-08-03 20:12:33 +0900

    [refactor] 커스텀 훅으로 데이터 처리 로직 분리

---
+++ src/hooks/diary/queries/useFetchRecentDiary.js
@@ -0,0 +1,40 @@
+import useRequireAuth from "../../auth/useRequireAuth";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { MINUTE } from "../../../utils/api/dateConverter";
+import useScreenOn from "../../common/useScreenOn";
+import { useEffect } from "react";
+import { generateRecentMock } from "../../../utils/mock/diary/recent";
+
+const useFetchRecentDiary = () => {
+  const { userId, isLogin } = useRequireAuth();
+
+  let { data, isFetching, fetchNextPage, isSuccess, hasNextPage } =
+    useInfiniteQuery({
+      queryKey: ["diary", "recent"],
+      queryFn: async ({ pageParam }) => {
+        const response = await generateRecentMock(pageParam, 6);
+        return response.data.result;
+      },
+      initialPageParam: 1,
+      getNextPageParam: (lastPage, allPages, lastPageParam) => {
+        if (lastPage.hasNext) return lastPageParam + 1;
+        return undefined;
+      },
+      enabled: isLogin,
+      staleTime: MINUTE,
+      gcTime: 5 * MINUTE,
+    });
+
+  const { isOnScreen, targetRef } = useScreenOn({
+    marginOffset: 100,
+  });
+
+  useEffect(() => {
+    if (!isOnScreen || isFetching || !hasNextPage) return;
+    fetchNextPage();
+  }, [isOnScreen, isFetching, isSuccess]);
+
+  return { data, isFetching, isSuccess, targetRef };
+};
+
+export default useFetchRecentDiary;
--- src/pages/mypage/RecentDiaries.js
+++ src/pages/mypage/RecentDiaries.js
@@ -1,42 +1,12 @@
-import React, { useEffect } from "react";
-import useRequireAuth from "../../hooks/auth/useRequireAuth";
-import { useInfiniteQuery } from "@tanstack/react-query";
-import { generateRecentMock } from "../../utils/mock/diary/diaryMockData";
-import { MINUTE } from "../../utils/api/dateConverter";
-import useScreenOn from "../../hooks/common/useScreenOn";
+import React from "react";
 import { range } from "../../utils/array/range";
 import { ResponseSkeleton } from "../../components/skeleton/ResponseSkeleton";
+import useFetchRecentDiary from "../../hooks/diary/queries/useFetchRecentDiary";
 
 export const RECENT_DIARIES_PAGE_PATH = "/recentdiaries";
 
 const RecentDiaries = () => {
-  const { userId, isLogin } = useRequireAuth();
-
-  let { data, isFetching, fetchNextPage, isSuccess, hasNextPage } =
-    useInfiniteQuery({
-      queryKey: ["diary", "recent"],
-      queryFn: async ({ pageParam }) => {
-        const response = await generateRecentMock(pageParam, 6);
-        return response.data.result;
-      },
-      initialPageParam: 1,
-      getNextPageParam: (lastPage, allPages, lastPageParam) => {
-        if (lastPage.hasNext) return lastPageParam + 1;
-        return undefined;
-      },
-      enabled: isLogin,
-      staleTime: MINUTE,
-      gcTime: 5 * MINUTE,
-    });
-
-  const { isOnScreen, targetRef } = useScreenOn({
-    marginOffset: 30,
-  });
-
-  useEffect(() => {
-    if (!isOnScreen || isFetching || !hasNextPage) return;
-    fetchNextPage();
-  }, [isOnScreen, isFetching, isSuccess]);
+  const { data, isFetching, isSuccess, targetRef } = useFetchRecentDiary();
 
   return (
     <div className="ml-10 md:ml-20 pb-6">
