de0a705b554b4f3bd9524280c9aed8a178f9b157
Won Chan Lee <twoone14@tukorea.ac.kr>
2024-08-04 21:32:33 +0900

    Feat/81 AI에게 그림 도움받기 기능 구현 (#121)
    * [refactor] 채팅기록 전역 상태로 관리
    
    * [feat] 이미지 생성 image.controller 작성
    
    * [feat] mutate 로 로직 분리
    
    * [feat] keyword 를 제외한 로직으로 AI 이미지 생성 api 구현
    
    * [feat] 마이페이지 api 키 입력 구현 (나중에 store 합칠 예정)
    
    * [refactor]
    useFetchKeyword.js -> useFetchKeywords.js 파일명 변경
    
    [refactor]
    useFetchKeyword.js -> useFetchKeywords.js 파일명 변경
    
    * [refactor] useFetchKeywords.js 에서 선택중인 키워드도 가져오게 변경
    
    * [refactor] 전역 상태를 이용한 키워드로 변경
    
    * [refactor] AI 챗 에러 상태 관리 코드 추가
    
    * [fix] 키워드 슬라이더 오류 해결
    
    * [refactor] 그림받기 각 오류 해결
    
    * [refactor] 그림받기 각 오류 해결2

---
+++ src/apis/image.controller.js
@@ -0,0 +1,31 @@
+import { Api } from "./common.controller";
+
+class ImageController extends Api {
+  // // 이미지 업로드
+  // uploadImg = async (imgData) => {
+  //   return await this.post("/image", {
+  //     data: imgData,
+  //     content_type: "multipart/form-data",
+  //   });
+  // };
+
+  //AI 이미지 생성
+  /**
+   * @return {Promise<AxiosResponse<ApiResponse<AiImageList>>>}
+   */
+  generateImage = async ({ password = "쓰레기값", prompt, n }) => {
+    return await this.post("/image/generate", { password, prompt, n });
+  };
+
+  checkState = async ({ taskId }) => {
+    return await this.get("/image/generate", { taskId });
+  };
+}
+
+export default new ImageController();
+
+/**
+ * @typedef {{
+ *   urls : string[],
+ * }} AiImageList
+ */
--- src/components/mypage/ApiKeyInput.jsx
+++ src/components/mypage/ApiKeyInput.jsx
@@ -1,20 +1,28 @@
 import AccordionWrapper from "../wrapper/AccordionWrapper";
 import Button from "../Button";
 import MyPageItem from "./MyPageItem";
+import useApiKeyStore from "../../stores/ApiKeyStore";
 
 const ApiKeyInput = ({ isOpen, onClick }) => {
+  const { apiKey, setApiKey } = useApiKeyStore((state) => state);
+
   return (
     <>
       <MyPageItem title={"API KEY 입력"} isOpen={isOpen} onClick={onClick} />
       <AccordionWrapper isOpen={isOpen}>
         <div className={"flex flex-col gap-4 items-end"}>
           <input
+            value={apiKey}
+            onChange={(e) => {
+              setApiKey(e.target.value);
+            }}
             type="text"
             className={
               "w-full outline-none border-2 border-black rounded-lg text-2xl p-2"
             }
           />
           <Button
+            onClick={onClick}
             className={"bg-[#6100C1] text-white p-2 px-10"}
             text={"완료"}
           />
