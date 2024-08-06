d4c1d75df26fd331002491cdb8580389b8797a76
Hj1218 <wjdguswl2341@naver.com>
2024-08-07 00:51:14 +0900

    [feat] 처음 로딩화면 스피너 추가

--- src/components/draw/KeywordReferenceDrawingViewer.js
+++ src/components/draw/KeywordReferenceDrawingViewer.js
@@ -1,19 +1,53 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, useCallback } from "react";
 import { useLocation } from "react-router-dom";
 import useFetchKeywordPhotos from "../../hooks/diary/queries/useFetchKeywordPhotos";
 import noDrawImg from "../../assets/remembrance/noKeyword.png";
-import img2 from "../../assets/remembrance/questionMark.png";
+import Spiner from "../../components/Spinner";
 
 const KeywordReferenceDrawingViewer = () => {
   const location = useLocation();
   const queryParams = new URLSearchParams(location.search);
-  const currentKeyword = queryParams.get("keyword");
-  const { imgUrls, isFetching, isSuccess, isError } =
-    useFetchKeywordPhotos(currentKeyword);
+  const currentKeyword = queryParams.get("keyword") || "default";
+  const {
+    imgUrls,
+    fetchNextPage,
+    hasNextPage,
+    isFetching,
+    isSuccess,
+    isError,
+  } = useFetchKeywordPhotos(currentKeyword);
 
+  const [isInitialLoading, setIsInitialLoading] = useState(true);
   const imgData = imgUrls.length > 0 ? imgUrls : [noDrawImg];
   const noImages = imgData.length === 1 && imgData[0] === noDrawImg;
   const [selectedDrawing, setSelectedDrawing] = useState(imgData[0]);
+
+  useEffect(() => {
+    if (imgUrls.length > 0 && selectedDrawing === noDrawImg) {
+      setSelectedDrawing(imgUrls[0]);
+    }
+  }, [imgUrls, selectedDrawing]);
+
+  useEffect(() => {
+    if (isFetching) {
+      if (isInitialLoading) {
+        return;
+      }
+      setIsInitialLoading(false);
+    } else {
+      if (isInitialLoading) {
+        setIsInitialLoading(false);
+      }
+    }
+  }, [isFetching]);
+
+  if (isInitialLoading) {
+    return (
+      <div className="w-full h-full flex justify-center items-center">
+        <Spiner color="#000000" />
+      </div>
+    );
+  }
 
   return (
     <>
@@ -23,6 +57,9 @@ const KeywordReferenceDrawingViewer = () => {
         setSelectedDrawing={setSelectedDrawing}
         imgUrls={imgData}
         noImages={noImages}
+        fetchNextPage={fetchNextPage}
+        hasNextPage={hasNextPage}
+        isFetching={isFetching}
       />
     </>
   );
@@ -49,19 +86,36 @@ const DrawingListScroller = ({
   setSelectedDrawing,
   imgUrls,
   noImages,
+  fetchNextPage,
+  hasNextPage,
+  isFetching,
 }) => {
   const scrollerRef = useRef(null);
+  const observerRef = useRef(null);
+
+  const observeLastImage = useCallback(
+    (node) => {
+      if (isFetching) return;
+      if (observerRef.current) observerRef.current.disconnect();
+      observerRef.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasNextPage) {
+          console.log("Fetching next page...");
+          fetchNextPage();
+        }
+      });
+      if (node) observerRef.current.observe(node);
+    },
+    [isFetching, hasNextPage, fetchNextPage]
+  );
 
   useEffect(() => {
     if (scrollerRef.current) {
-      //선택된 그림 요소 찾기
       const selectedElement = scrollerRef.current.querySelector(
         `img[src='${selectedDrawing}']`
       );
       if (selectedElement) {
-        const containerWidth = scrollerRef.current.offsetWidth; //컨테이너 너비
-        const selectedElementWidth = selectedElement.offsetWidth; //선택된 그림 너비
-        // 선택된 그림의 offsetLeft - (컨테이너 너비)/2 + 선택된 그림 너비 절반 = 컨테이너의 중앙
+        const containerWidth = scrollerRef.current.offsetWidth;
+        const selectedElementWidth = selectedElement.offsetWidth;
         const scrollPosition =
           selectedElement.offsetLeft -
           containerWidth / 2 +
@@ -80,19 +134,35 @@ const DrawingListScroller = ({
       className="flex overflow-x-scroll scrollbar-hide pt-10"
     >
       {!noImages ? (
-        imgUrls.map((img, index) => (
-          <img
-            key={index}
-            src={img}
-            className={`transition-transform duration-500 w-20 md:w-32 aspect-square ml-6 md:ml-10 cursor-pointer
-              ${
+        imgUrls.map((img, index) => {
+          if (index === imgUrls.length - 1) {
+            return (
+              <img
+                key={index}
+                src={img}
+                ref={observeLastImage}
+                className={`transition-transform duration-500 w-20 md:w-32 aspect-square ml-6 md:ml-10 cursor-pointer ${
+                  selectedDrawing === img
+                    ? "-translate-y-10 border-2 border-[#6100C1]"
+                    : "border border-black"
+                }`}
+                onClick={() => setSelectedDrawing(img)}
+              />
+            );
+          }
+          return (
+            <img
+              key={index}
+              src={img}
+              className={`transition-transform duration-500 w-20 md:w-32 aspect-square ml-6 md:ml-10 cursor-pointer ${
                 selectedDrawing === img
                   ? "-translate-y-10 border-2 border-[#6100C1]"
                   : "border border-black"
               }`}
-            onClick={() => setSelectedDrawing(img)}
-          />
-        ))
+              onClick={() => setSelectedDrawing(img)}
+            />
+          );
+        })
       ) : (
         <div className="flex items-center justify-center w-full h-full text-xl md:text-3xl">
           <p>키워드 그림이 존재하지 않습니다.</p>
