c4226a744e535e76b963b5c7ed47710be546615f
Hj1218 <wjdguswl2341@naver.com>
2024-08-07 00:50:22 +0900

    [feat] 페이지 넘길 수 있도록 수정

--- src/hooks/diary/queries/useFetchKeywordPhotos.js
+++ src/hooks/diary/queries/useFetchKeywordPhotos.js
@@ -1,27 +1,34 @@
-import { useQuery } from "@tanstack/react-query";
+import { useInfiniteQuery } from "@tanstack/react-query";
 import DiaryController from "../../../apis/diary.controller";
 
-const useFetchKeywordPhotos = (keyword, page = 1, pageSize = 6) => {
-  const {
-    data = { imgUrls: [] },
+const useFetchKeywordPhotos = (keyword) => {
+  const { data, fetchNextPage, hasNextPage, isFetching, isSuccess, isError } =
+    useInfiniteQuery({
+      queryKey: [keyword],
+      queryFn: async ({ pageParam = 1 }) => {
+        const response = await DiaryController.getKeywordPhotos({
+          keyword,
+          page: pageParam,
+          pageSize: 6,
+        });
+        return response.data.result;
+      },
+      getNextPageParam: (lastPage, allPages) => {
+        return lastPage.imgUrls.length > 0 ? allPages.length + 1 : undefined;
+      },
+      enabled: !!keyword,
+    });
+
+  const imgUrls = data?.pages.flatMap((page) => page.imgUrls) || [];
+
+  return {
+    imgUrls,
+    fetchNextPage,
+    hasNextPage,
     isFetching,
     isSuccess,
     isError,
-  } = useQuery({
-    queryKey: [keyword, page],
-    queryFn: async () => {
-      const response = await DiaryController.getKeywordPhotos({
-        keyword,
-        page,
-        pageSize,
-      });
-      return response.data.result;
-    },
-    keepPreviousData: true,
-    enabled: !!keyword,
-  });
-
-  return { imgUrls: data.imgUrls, isFetching, isSuccess, isError };
+  };
 };
 
 export default useFetchKeywordPhotos;
