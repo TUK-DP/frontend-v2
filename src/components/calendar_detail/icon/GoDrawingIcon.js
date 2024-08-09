e67ca2028137171a03f69b5024b70008930d13ad
이원찬 <twoone14@tukorea.ac.kr>
2024-08-09 17:54:20 +0900

    [publishing] 오른쪽처럼 멘트 수정 "그림이 없네 그림도 그려볼까요"
    [publishing] 오른쪽처럼 멘트 수정 "그림이 없네 그림도 그려볼까요"

--- src/components/calendar_detail/icon/GoDrawingIcon.js
+++ src/components/calendar_detail/icon/GoDrawingIcon.js
@@ -2,8 +2,8 @@ export const GoDrawingIcon = ({ className, ...props }) => {
   return (
     <div className={className} {...props}>
       <svg
-        width="64"
-        height="64"
+        width="100%"
+        height="100%"
         viewBox="0 0 64 64"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
