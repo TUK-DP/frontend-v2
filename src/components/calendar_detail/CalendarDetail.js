12f5fe7d09fafabfed2b309fc9460f715916a39a
이원찬 <twoone14@tukorea.ac.kr>
2024-08-09 17:54:20 +0900

    [publishing] 오른쪽처럼 멘트 수정 "그림이 없네 그림도 그려볼까요"
    [publishing] 오른쪽처럼 멘트 수정 "그림이 없네 그림도 그려볼까요"

--- src/components/calendar_detail/CalendarDetail.js
+++ src/components/calendar_detail/CalendarDetail.js
@@ -52,7 +52,7 @@ const WhatHappenSection = () => {
 
   return (
     <>
-      <div className={"text-3xl text-white font-bold mb-8"}>
+      <div className={"text-3xl text-white font-bold"}>
         무슨 일이 있었나요?
       </div>
       <div className={`flex justify-between gap-8 mobile:gap-4`}>
@@ -66,8 +66,8 @@ const WhatHappenSection = () => {
 const GoDrawingSection = () => {
   return (
     <>
-      <div className={"text-3xl text-white font-bold mb-8"}>
-        그림도 그려 볼까요?
+      <div className={"text-3xl text-white font-bold"}>
+        그림이 없네요 <br/> 그림도 그려 볼까요?
       </div>
       <GoDrawingButton />
     </>
@@ -79,9 +79,9 @@ const GoDrawingButton = () => {
   return (
     <div
       onClick={goDiaryDrawPage}
-      className={"rounded-2xl bg-white flex p-4 cursor-pointer"}
+      className={"rounded-2xl bg-white flex p-8 cursor-pointer h-40 mobile:h-32"}
     >
-      <GoDrawingIcon className={"w-20 mr-12 " + "mobile:w-14 mobile:mr-6"} />
+      <GoDrawingIcon className={"h-full aspect-square mr-12 " + "mobile:mr-6"} />
       <div
         className={
           "flex flex-col justify-between font-bold text-2xl mobile:text-xl"
