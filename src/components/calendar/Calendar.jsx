a8ef49b7640f3d95c6128dbad61fc9e71d7cc769
이원찬 <twoone14@tukorea.ac.kr>
2024-08-10 16:45:26 +0900

    [feat] 캘린더 날짜 선택시 쿼리스트링으로 날짜 데이터를 저장하여 새로고침시 날짜가 변경되지 않게 변경
    ˆ

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -1,7 +1,38 @@
 import { CalendarMonthSelector } from "./CalendarMonthSelector";
 import { CalendarGrid } from "./CalendarGrid";
+import { useCalendarStore } from "../../stores/CalendarStore";
+import { useEffect } from "react";
+import { useSearchParams } from "react-router-dom";
 
 export const Calendar = () => {
+  const [searchParams, setQuery] = useSearchParams();
+  const query = Object.fromEntries(searchParams.entries());
+  const { selectedDate, setSelectedDate } = useCalendarStore((state) => state);
+
+  // 초기 선택된 날짜가 쿼리로 들어왔을 때
+  useEffect(() => {
+    const { year, month, day } = query;
+    if (!year || !month || !day) return;
+
+    setSelectedDate({
+      year: parseInt(year),
+      month: parseInt(month),
+      day: parseInt(day),
+    });
+  }, []);
+
+  // 날짜를 선택 할때 마다 쿼리를 변경
+  useEffect(() => {
+    setQuery(
+      { ...selectedDate },
+      {
+        replace: true,
+      }
+    );
+  }, [selectedDate]);
+
+  useEffect(() => {}, []);
+
   return (
     <>
       <CalendarMonthSelector />
