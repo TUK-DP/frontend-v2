33d63ed43317d601bcd513396a2a2d3a9888dff3
이원찬 <twoone14@tukorea.ac.kr>
2024-08-10 15:43:21 +0900

    [feat] 삭제 기능 구현과 삭제 이전 모달창 추가

---
+++ src/hooks/diaryDetail/queries/useDeleteDiary.js
@@ -0,0 +1,59 @@
+import { useCalendarStore } from "../../../stores/CalendarStore";
+import {
+  useIsMutating,
+  useMutation,
+  useQueryClient,
+} from "@tanstack/react-query";
+import DiaryController from "../../../apis/diary.controller";
+import { yearMonthToDashString } from "../../../utils/api/dateConverter";
+import { diaryCheckQueryKey } from "../../diary/queries/useFetchDiaryChecks";
+import useFetchDiary, {
+  diaryQueryKey,
+} from "../../diary/queries/useFetchDiary";
+
+const useDeleteDiary = () => {
+  const { selectedDate, selectedYearMonth } = useCalendarStore(
+    (state) => state
+  );
+
+  let queryClient = useQueryClient();
+  const { diary } = useFetchDiary();
+
+  const { mutate } = useMutation({
+    mutationKey: ["diary", "delete", selectedDate],
+    mutationFn: async (content) => {
+      return await DiaryController.deleteDiary({
+        diaryId: diary.diaryId,
+      });
+    },
+    onMutate: async (content) => {
+      queryClient.setQueryData(
+        diaryCheckQueryKey(selectedYearMonth),
+        (old) => ({
+          [yearMonthToDashString(selectedYearMonth)]: {
+            ...old[yearMonthToDashString(selectedYearMonth)],
+            [selectedDate.day]: { isExist: false },
+          },
+        })
+      );
+
+      await queryClient.invalidateQueries({
+        queryKey: diaryQueryKey(selectedDate),
+      });
+    },
+    onSuccess: async () => {
+      // 일기 삭제 성공시
+      await queryClient.invalidateQueries({
+        queryKey: diaryQueryKey(selectedDate),
+      });
+    },
+  });
+
+  const isMutating = useIsMutating({
+    mutationKey: ["diary", "delete", selectedDate],
+  });
+
+  return { mutate, isMutating: isMutating === 1 };
+};
+
+export default useDeleteDiary;
--- src/pages/diaryDetails/DiaryControlButton.jsx
+++ src/pages/diaryDetails/DiaryControlButton.jsx
@@ -2,6 +2,11 @@ import Spinner from "../../components/Spinner";
 import useCreateDiary from "../../hooks/diaryDetail/queries/useCreateDiary";
 import useUpdateDiary from "../../hooks/diaryDetail/queries/useUpdateDiary";
 import useFetchDiary from "../../hooks/diary/queries/useFetchDiary";
+import useDeleteDiary from "../../hooks/diaryDetail/queries/useDeleteDiary";
+import { useNavigate } from "react-router-dom";
+import { DIARY_PAGE_PATH } from "../diarys/Diary";
+import { useEffect, useState } from "react";
+import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
 
 /**
  * @param content {string}
@@ -21,6 +26,8 @@ const DiaryControlButton = ({
   const create = useCreateDiary();
   const update = useUpdateDiary();
 
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+
   const isMutating = create.isMutating || update.isMutating;
 
   return (
@@ -30,19 +37,90 @@ const DiaryControlButton = ({
           ? "키워드 추출중입니다!"
           : controlState.controlButtonMessage}
       </div>
-      <button
-        disabled={isMutating}
-        onClick={() =>
-          handleClick(() => {
-            isDiaryExist ? update.mutate(content) : create.mutate(content);
-          })
+      <div className={"w-full flex gap-12 mobile:gap-4"}>
+        <button
+          disabled={isMutating}
+          onClick={() =>
+            handleClick(() => {
+              isDiaryExist ? update.mutate(content) : create.mutate(content);
+            })
+          }
+          className={`flex-1 border-2 bg-secondary-400 rounded-lg text-2xl py-3 ${className}`}
+          {...props}
+        >
+          {isMutating && <Spinner color={"black"} />}
+          {!isMutating && controlState.controlButtonText}
+        </button>
+        {isDiaryExist && !isMutating && (
+          <button
+            onClick={() => {
+              setIsDeleteModalOpen(true);
+              // disableBodyScroll(document.body);
+            }}
+            className={`flex-1 border-2 bg-red-500 text-white rounded-lg text-2xl py-3 ${className}`}
+          >
+            삭제
+          </button>
+        )}
+        {isDeleteModalOpen && (
+          <DeleteModal
+            isDeleteModalOpen={isDeleteModalOpen}
+            setIsDeleteModalOpen={setIsDeleteModalOpen}
+          />
+        )}
+      </div>
+    </>
+  );
+};
+
+const DeleteModal = ({ isDeleteModalOpen, setIsDeleteModalOpen }) => {
+  useEffect(() => {
+    disableBodyScroll(document.body);
+
+    return () => {
+      enableBodyScroll(document.body);
+    };
+  }, []);
+
+  const navigate = useNavigate();
+  const _delete = useDeleteDiary();
+
+  return (
+    <>
+      <div // This div is used to prevent the user from clicking the background
+        // onDrag={(e) => e.preventDefault()}
+        onClick={(e) => setIsDeleteModalOpen(false)}
+        className={"fixed w-screen h-screen inset-0 bg-black opacity-30"}
+      />
+      <div
+        className={
+          "fixed -translate-y-1/2 top-1/2 left-1/2 -translate-x-1/2 " +
+          "bg-white border-2 rounded-lg w-[80%] h-80 " +
+          "flex flex-col items-center " +
+          "text-2xl px-4 py-10 z-10"
         }
-        className={`border-2 bg-secondary-400 rounded-lg text-2xl py-3 ${className}`}
-        {...props}
       >
-        {isMutating && <Spinner color={"black"} />}
-        {!isMutating && controlState.controlButtonText}
-      </button>
+        <div className={"flex-1 flex items-center"}>
+          일기를 삭제하시겠습니까?
+        </div>
+        <div className={"w-full flex gap-4 justify-between"}>
+          <button
+            onClick={() => setIsDeleteModalOpen(false)}
+            className={"flex-1 border-2 p-2 rounded-lg"}
+          >
+            취소
+          </button>
+          <button
+            onClick={() => {
+              _delete.mutate();
+              navigate(DIARY_PAGE_PATH);
+            }}
+            className={"flex-1 border-2 p-2 rounded-lg bg-red-500 text-white"}
+          >
+            삭제
+          </button>
+        </div>
+      </div>
     </>
   );
 };
