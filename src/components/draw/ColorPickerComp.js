8d8fc183898566f040c39005b2679886bbf7e50a
Hj1218 <wjdguswl2341@naver.com>
2024-08-10 18:57:38 +0900

    [publishing] 필요없는 코드 삭제

--- src/components/draw/ColorPickerComp.js
+++ src/components/draw/ColorPickerComp.js
@@ -25,7 +25,6 @@ const ColorPickerComp = ({ setColorPickerOpen }) => {
       enableBodyScroll(document.body);
     };
   }, []);
-  console.log(colorPickerHeight);
 
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50">
