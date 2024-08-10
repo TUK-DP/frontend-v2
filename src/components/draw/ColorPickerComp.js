906d965176e540e4d0f635b2e7123c3402941e09
Hj1218 <wjdguswl2341@naver.com>
2024-08-10 18:52:57 +0900

    [publishing] 모달 닫는 버튼 추가

--- src/components/draw/ColorPickerComp.js
+++ src/components/draw/ColorPickerComp.js
@@ -24,27 +24,35 @@ const ColorPickerComp = ({ setColorPickerOpen }) => {
 
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg w-[20rem] md:w-[35rem] h-fit">
-        <div className="w-full font-bold text-xl md:text-3xl flex justify-center mb-3">
-          원하는 색상을 선택하세요
-        </div>
-        <ColorPicker
-          color={color}
-          onChange={setColor}
-          hideInput={["rgb", "hsv", "hex"]} // 옵션 숨기기
-          height={colorPickerHeight}
-        />
-        <div className="flex items-center justify-between mt-6">
-          <ColorState color={color} />
-          <button
-            className="bg-[#6100C1] hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded-md shadow-md transition duration-300 md:text-2xl"
-            onClick={() => {
-              setDrawingTools({ ...drawingTools, color: color.hex });
-              setColorPickerOpen(false);
-            }}
-          >
-            선택
-          </button>
+      <div className="bg-white rounded-lg shadow-lg w-[20rem] md:w-[35rem]">
+        <button
+          className="text-[#6100C1] font-semibold text-lg md:text-2xl w-full text-end pr-4 pt-2 md:pt-4"
+          onClick={() => setColorPickerOpen(false)}
+        >
+          취소
+        </button>
+        <div className="pt-3 p-6">
+          <div className="w-full font-bold text-xl md:text-3xl flex justify-center mb-3">
+            원하는 색상을 선택하세요
+          </div>
+          <ColorPicker
+            color={color}
+            onChange={setColor}
+            hideInput={["rgb", "hsv", "hex"]} // 옵션 숨기기
+            height={colorPickerHeight}
+          />
+          <div className="flex items-center justify-between mt-6">
+            <ColorState color={color} />
+            <button
+              className="bg-[#6100C1] hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded-md shadow-md transition duration-300 md:text-2xl"
+              onClick={() => {
+                setDrawingTools({ ...drawingTools, color: color.hex });
+                setColorPickerOpen(false);
+              }}
+            >
+              선택
+            </button>
+          </div>
         </div>
       </div>
     </div>
