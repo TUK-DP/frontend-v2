e2cd14417db9a2c11166dc464dda4b0d53941332
정현지 <114800998+Hj1218@users.noreply.github.com>
2024-08-12 16:20:30 +0900

    Publishing/145 그리기 도구 수정 (#154)
    * [publishing] 버튼 크기 반응형 추가
    
    * [publishing] 내용만큼 모달 height fit 조정
    
    * [publishing] 모달 닫는 버튼 추가
    
    * [publishing] 모달 뒷배경 스크롤 막기
    
    * [publishing] 필요없는 코드 삭제
    
    * [publishing] 컬리피커 모달 수정

--- src/components/draw/ColorPickerComp.js
+++ src/components/draw/ColorPickerComp.js
@@ -1,8 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { ColorPicker, useColor } from "react-color-palette";
 import "react-color-palette/css";
+import "../../styles/colorPicker.css";
 import { pickerHeight, mdPickerHeight } from "../../constants/size";
 import { useDrawingToolStore } from "../../stores/DrawingToolStore";
+import { disableBodyScroll, enableBodyScroll } from "body-scroll-lock";
+import { IoClose } from "react-icons/io5";
 
 const ColorPickerComp = ({ setColorPickerOpen }) => {
   const { drawingTools, setDrawingTools } = useDrawingToolStore();
@@ -19,32 +22,43 @@ const ColorPickerComp = ({ setColorPickerOpen }) => {
     };
 
     updateHeight();
+    disableBodyScroll(document.body);
+    return () => {
+      enableBodyScroll(document.body);
+    };
   }, []);
-  console.log(colorPickerHeight);
 
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg w-[20rem] md:w-[35rem] md:h-[30rem]">
-        <div className="w-full font-bold text-xl md:text-3xl flex justify-center mb-3">
-          원하는 색상을 선택하세요
-        </div>
-        <ColorPicker
-          color={color}
-          onChange={setColor}
-          hideInput={["rgb", "hsv", "hex"]} // 옵션 숨기기
-          height={colorPickerHeight}
-        />
-        <div className="flex items-center justify-between mt-6">
-          <ColorState color={color} />
-          <button
-            className="bg-[#6100C1] hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded-md shadow-md transition duration-300 md:text-2xl"
-            onClick={() => {
-              setDrawingTools({ ...drawingTools, color: color.hex });
-              setColorPickerOpen(false);
-            }}
-          >
-            선택
-          </button>
+      <div className="bg-white rounded-lg shadow-lg w-[20rem] md:w-[35rem]">
+        <button
+          className="text-[#6100C1] font-semibold w-full flex justify-end pr-2 md:pr-4 pt-2 md:pt-4"
+          onClick={() => setColorPickerOpen(false)}
+        >
+          <IoClose className="text-lg md:text-4xl" />
+        </button>
+        <div className="pt-3 p-6">
+          <div className="w-full font-bold text-xl md:text-3xl flex justify-center mb-3">
+            원하는 색상을 선택하세요
+          </div>
+          <ColorPicker
+            color={color}
+            onChange={setColor}
+            hideInput={["rgb", "hsv", "hex"]} // 옵션 숨기기
+            height={colorPickerHeight}
+          />
+          <div className="flex items-center justify-between mt-6">
+            <ColorState color={color} />
+            <button
+              className="bg-[#6100C1] hover:bg-blue-600 text-white font-semibold py-2 px-6 rounded-md shadow-md transition duration-300 md:text-2xl"
+              onClick={() => {
+                setDrawingTools({ ...drawingTools, color: color.hex });
+                setColorPickerOpen(false);
+              }}
+            >
+              선택
+            </button>
+          </div>
         </div>
       </div>
     </div>
