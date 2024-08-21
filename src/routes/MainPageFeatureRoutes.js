47b8055ab185369facf5d01541487d0569c20caa
이원찬 <twoone14@tukorea.ac.kr>
2024-08-22 00:26:04 +0900

    [fix] 중복되던 치매 센터 라우팅 제거

--- src/routes/MainPageFeatureRoutes.js
+++ src/routes/MainPageFeatureRoutes.js
@@ -1,6 +1,3 @@
-import DementiaCenter, {
-  DEMENTIA_CENTER_PAGE_PATH,
-} from "../pages/dementiaCenter/DementiaCenter";
 import { Route } from "react-router-dom";
 import React from "react";
 import Gym, { GYM_PAGE_PATH } from "../pages/gym/Gym";
@@ -21,7 +18,6 @@ import ColorMatchIntro, {
 const MainPageFeatureRoutes = () => {
   return (
     <>
-      <Route path={DEMENTIA_CENTER_PAGE_PATH} element={<DementiaCenter />} />
       <Route path={GYM_PAGE_PATH} element={<Gym />} />
       {/* 게임관련 라우팅 */}
       <Route path={SELECT_GAMES_PAGE_PATH} element={<SelectGames />} />
